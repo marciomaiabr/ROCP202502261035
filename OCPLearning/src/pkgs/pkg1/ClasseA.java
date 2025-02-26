a58943b6e15042d312b817f113d593804c0577f1
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-02-26 11:01:45 -0400

    inicio...

---
+++ OCPLearning/src/pkgs/pkg1/ClasseA.java
@@ -0,0 +1,5 @@
+package pkgs.pkg1;
+
+public class ClasseA {
+
+}
---
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -0,0 +1,12 @@
+package pkgs.pkgExes;
+
+import pkgs.pkg1.ClasseA;
+
+public class Exe001 {
+
+	public static void main(String[] args) {
+		ClasseA ca = null;
+		System.out.println(ca);
+	}
+
+}
