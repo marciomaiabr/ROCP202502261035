adc7fc7d09c210ae46fec734ac9c2ed3718d2fa0
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-03 09:48:25 -0400

    cleaning

--- OCPLearning/src/pkgs/pkg1/Animal.java
+++ OCPLearning/src/pkgs/pkg1/Animal.java
@@ -1,9 +1,5 @@
 package pkgs.pkg1;
 
-import pkgs.pkg2.LetraGrega;
-import pkgs.pkg2.Omega;
-import pkgs.pkg3.ExceptionAA;
-
 public class Animal{
 
 	void locomover() {}
--- OCPLearning/src/pkgs/pkg1/Cavalo.java
+++ OCPLearning/src/pkgs/pkg1/Cavalo.java
@@ -1,14 +1,5 @@
 package pkgs.pkg1;
 
-import pkgs.pkg2.LetraGrega;
-import pkgs.pkg2.Omega;
-import pkgs.pkg3.ExceptionA;
-import pkgs.pkg3.ExceptionAA;
-import pkgs.pkg3.ExceptionAAA;
-import pkgs.pkg3.ExceptionRTA;
-
 public class Cavalo extends Animal{
-
-	static void locomover() {}
 
 }
