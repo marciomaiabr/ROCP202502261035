f42745e1de76f905c8b15acaf9e381399c0fb442
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-03 09:23:11 -0400

    OK metodo herdado disparando a mesma exceção subtipo do pai

--- OCPLearning/src/pkgs/pkg1/Cavalo.java
+++ OCPLearning/src/pkgs/pkg1/Cavalo.java
@@ -3,10 +3,11 @@ package pkgs.pkg1;
 import pkgs.pkg2.LetraGrega;
 import pkgs.pkg2.Omega;
 import pkgs.pkg3.ExceptionAA;
+import pkgs.pkg3.ExceptionAAA;
 
 public class Cavalo extends Animal{
 
 	@Override
-	void locomover() throws ExceptionAA {}
+	void locomover() throws ExceptionAAA {}
 
 }
