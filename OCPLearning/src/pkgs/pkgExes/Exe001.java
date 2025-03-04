6a79388e34472e334bc4813d5eea4558fce1a33c
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-04 10:53:37 -0400

    cleanning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,21 +1,5 @@
 package pkgs.pkgExes;
 
-interface I1{
-	void mtc1();
-}
-
-interface I11 extends I1{
-	default void mtc1() {System.out.println("I11.mtc1()");};
-}
-
-interface I12 extends I1{
-	default void mtc1() {System.out.println("I12.mtc1()");};
-}
-
-abstract class C1 implements I11 , I12 {
-	public void mtc1() {System.out.println("C1.mtc1()");};
-}
-
 public class Exe001 {
 
 	public static void main(String[] args) {
