c475afb1a20fccdfd037b73766e9375f1af23e69
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-05 12:26:48 -0400

    using Show class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,5 +1,7 @@
 package pkgs.pkgExes;
 
+import pkgs.pkg6.Show;
+
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -8,6 +10,8 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		Show.ticketAgentBooks("1A");
+		Show.ticketAgentBooks("1A");
 	}
 
 }
