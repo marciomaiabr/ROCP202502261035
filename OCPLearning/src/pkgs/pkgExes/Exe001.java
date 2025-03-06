3d28077ca6028b835371d0f4325ab393710201b0
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-06 11:21:36 -0400

    using ADSR class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,5 +1,7 @@
 package pkgs.pkgExes;
 
+import pkgs.pkg7.ADSR;
+
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -8,6 +10,13 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		StringBuilder sb = new StringBuilder("a1 ");
+		ADSR a1 = new ADSR(sb, 5, 7);
+		ADSR a2 = a1.getADSR();
+		System.out.println(a1.getName());
+		sb.append("alter the name ");
+		System.out.println(a1.getName());
+		System.out.println(a2.getName());
 	}
 
 }
