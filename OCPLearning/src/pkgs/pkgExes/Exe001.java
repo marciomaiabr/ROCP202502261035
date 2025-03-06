5023dcb7fbb05fdc43635ca46ccfe8a778aadd2a
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-06 16:23:18 -0400

    para acessar CONSTANTES Java NAO executa NENHUM mtd ou bloco da classe ou supertype

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,6 +8,9 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		System.out.println(pkgs.pkg1.Animal.X);
+		System.out.println(pkgs.pkg1.pkg11.Ponei.X);
+		System.out.println(pkgs.pkg1.pkg11.Ponei.Z);
 	}
 
 }
