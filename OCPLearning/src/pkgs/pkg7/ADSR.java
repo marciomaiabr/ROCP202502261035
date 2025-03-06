a78bb70498f68ba70c7e0bfa37dfb2e61ab7a10a
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-06 11:18:36 -0400

    adding ADSR class

---
+++ OCPLearning/src/pkgs/pkg7/ADSR.java
@@ -0,0 +1,24 @@
+package pkgs.pkg7;
+
+public final class ADSR {
+
+	private final StringBuilder name;
+	private final int attack;
+	private final int decay;
+
+	public ADSR(StringBuilder name, int attack, int decay) {
+		super();
+		this.name = name;
+		this.attack = attack;
+		this.decay = decay;
+	}
+
+	public StringBuilder getName() {
+		return new StringBuilder(name);
+	}
+
+	public ADSR getADSR() {
+		return this;
+	}
+
+}
