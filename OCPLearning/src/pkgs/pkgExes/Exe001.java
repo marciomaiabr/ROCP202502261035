f95cd6e78e263cc321a3a85c5d761c204956f27d
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-06 11:13:59 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,7 +1,5 @@
 package pkgs.pkgExes;
 
-import pkgs.pkg6.ShowEnum;
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -10,8 +8,6 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		ShowEnum.ticketAgentBooks("1A");
-		ShowEnum.ticketAgentBooks("1A");
 	}
 
 }
