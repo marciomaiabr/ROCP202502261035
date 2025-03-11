e9f714e4148dba845dedc1582c320cc3532de333
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-11 08:04:39 -0400

    metodos staticos

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,13 +8,13 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		pkgs.pkg1.Cavalo o = null;
-		o.sm2();
+		pkgs.pkg1.pkg11.Ponei o = null;
+		o.sm4();
 	}
 
 }
 
 /*
-quando executado um mtd sttc de uma classe que chama outro mtd sttc que esteja REDEFINIDO nessa classe,
-o java executa o mtd REDEFINIDO
+quando executado um mtd sttc de uma classe que chama outro mtd sttc que NAO esteja REDEFINIDO nessa classe,
+o java executa o mtd REDEFINIDO no supertype
 */
