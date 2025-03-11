e2d3c4276dc04c66b0311055776be99d8057aba4
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-11 06:53:05 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,15 +8,10 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		try {
-			Class.forName("pkgs.pkg1.Animal");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 }
 
 /*
-blocos staticos em classe sem supertype sao executados em sequencia conforme ordem que estao escritos
+
 */
