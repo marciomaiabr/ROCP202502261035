a2c2ea815c5987d7de7898d470504417388a9c74
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-11 07:22:38 -0400

    blocos staticos

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,10 +8,13 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		pkgs.pkg1.pkg11.Ponei p = null;
+		p.smAnimal();
 	}
 
 }
 
 /*
-
+quando executado somente o metodo statico de uma superclass,
+o java nem executa os blocos staticos da classe da variavel declarada 
 */
