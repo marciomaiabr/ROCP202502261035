f800cba973427e3542cd56e4177c26f533848cef
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-13 19:14:54 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -15,8 +15,5 @@ public class Exe001 {
 }
 
 /*
-em assertions enabled
-quando expression1, ou seja, o que esta entre parenteses do assert FALSE
-a execucao DISPARA UMA java.lang.AssertionError
-pra memorizar, assert FALSE quer dizer que aquilo que está em expression1 NAO BATEU com aquilo que vc esperava
+
 */
