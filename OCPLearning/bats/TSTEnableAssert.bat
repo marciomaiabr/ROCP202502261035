426f4bed839772a1969cb81e175a07a606e1746b
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-13 18:39:09 -0400

    tst enable assertions with -enableassertions

---
+++ OCPLearning/bats/TSTEnableAssert.bat
@@ -0,0 +1,6 @@
+cls
+rmdir /s /q "C:\temp\mmbin"
+mkdir "C:\temp\mmbin"
+javac -d "C:/temp/mmbin" -cp .;..;../src/; ../src/pkgs/pkgExes/Exe001.java
+java -enableassertions -cp .;..;C:/temp/mmbin; pkgs.pkgExes.Exe001
+pause
