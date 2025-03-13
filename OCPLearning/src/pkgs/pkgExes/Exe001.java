d2c07de49572b43e74eaad81cafbdfd5c0dfa1e0
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-13 19:40:17 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,17 +8,10 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		int x = 5;
-		try {
-			assert(false):"[x="+(x)+"]";
-		} catch (Throwable t) {
-			System.out.println("MSG disparada: "+t.getMessage());
-		}
-		System.out.println("/Exe001.m1()");
 	}
 
 }
 
 /*
-capturar assertion com try/catch E POSSIVEL pois dispara AssertionError ou seja um ERROR ou seja um Throwable
+
 */
