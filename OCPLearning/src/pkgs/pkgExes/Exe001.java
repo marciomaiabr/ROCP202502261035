0a432ba94441deedc006bc8c22656983a487457c
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-13 18:59:04 -0400

    assertions tst

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,10 +8,14 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		assert(true);
+		System.out.println("/Exe001.m1()");
 	}
 
 }
 
 /*
-
+em assertions enabled
+quando expression1, ou seja, o que esta entre parenteses do assert TRUE
+a execucao OCORRE SEM PROBLEMAS
 */
