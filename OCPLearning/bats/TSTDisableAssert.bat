d9a8f9309bbe7c0324e538d293f0f1ae8fc4f7cd
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-13 20:14:54 -0400

    tst disable assertions with -da

--- OCPLearning/bats/TSTDisableAssert.bat
+++ OCPLearning/bats/TSTDisableAssert.bat
@@ -2,5 +2,5 @@ cls
 rmdir /s /q "C:\temp\mmbin"
 mkdir "C:\temp\mmbin"
 javac -d "C:/temp/mmbin" -cp .;..;../src/; ../src/pkgs/pkgExes/Exe001.java
-java -disableassertions -cp .;..;C:/temp/mmbin; pkgs.pkgExes.Exe001
+java -da -cp .;..;C:/temp/mmbin; pkgs.pkgExes.Exe001
 pause
