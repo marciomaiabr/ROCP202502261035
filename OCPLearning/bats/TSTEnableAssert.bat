b755d640b9972ec7a3f80fbacb079ee3b0405384
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-14 08:50:13 -0400

    enable assertions NOT enable in single package

--- OCPLearning/bats/TSTEnableAssert.bat
+++ OCPLearning/bats/TSTEnableAssert.bat
@@ -2,5 +2,5 @@ cls
 rmdir /s /q "C:\temp\mmbin"
 mkdir "C:\temp\mmbin"
 javac -d "C:/temp/mmbin" -cp .;..;../src/; ../src/pkgs/pkgExes/Exe001.java
-java -ea:pkgs.pkg8.pkg81.pkg811.ClassePKG8B -cp .;..;C:/temp/mmbin; pkgs.pkgExes.Exe001
+java -ea:pkgs.pkg8.pkg81.pkg811 -cp .;..;C:/temp/mmbin; pkgs.pkgExes.Exe001
 pause
