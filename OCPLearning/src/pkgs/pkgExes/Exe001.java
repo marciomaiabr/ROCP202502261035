8b85c2e0e0132b8e8f41ca1d709a6dedc07665f1
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-14 08:28:14 -0400

    assertions tst

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,7 +8,21 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		new pkgs.pkg8.pkg81.pkg811.ClassePKG8B().mtd();
+		try {
+			new pkgs.pkg8.pkg81.ClassePKG8A().mtd();
+		} catch (Throwable t) {
+			System.out.println(t.getMessage());
+		}
+		try {
+			new pkgs.pkg8.pkg81.pkg811.ClassePKG8B().mtd();
+		} catch (Throwable t) {
+			System.out.println(t.getMessage());
+		}
+		try {
+			new pkgs.pkg8.pkg81.pkg811.pkg8111.ClassePKG8C().mtd();
+		} catch (Throwable t) {
+			System.out.println(t.getMessage());
+		}
 		System.out.println("/Exe001.m1()");
 	}
 
