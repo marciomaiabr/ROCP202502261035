f70a9778d1826258dd82a6fc66a8ba20021f9a8c
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-15 18:49:12 -0400

    OK catch (Exception e) pois o java sabe q no throws tem as exceptions disparadas no body do try

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -2,11 +2,20 @@ package pkgs.pkgExes;
 
 import pkgs.pkg3.ExceptionA;
 import pkgs.pkg3.ExceptionB;
+import pkgs.pkg3.ExceptionC;
 
-class ClasseQQ{
+class ClasseQQ {
+
+	public void throwsExceptionA() throws ExceptionA {}
+	public void throwsExceptionAExceptionB() throws ExceptionA, ExceptionB {}
+	public void throwsExceptionAExceptionBExceptionC() throws ExceptionA, ExceptionB, ExceptionC {}
 
 	public void mtd() throws ExceptionA, ExceptionB {
-		try {} catch (ExceptionA e) {}
+		try {
+			throwsExceptionAExceptionB();
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 }
@@ -19,11 +28,16 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		try {
+			new ClasseQQ().mtd();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("/Exe001.m1()");
 	}
 
 }
 
 /*
-
+OK pois o java sabe q no throws tem as exceptions disparadas no body do try
 */
