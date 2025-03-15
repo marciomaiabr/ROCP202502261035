d49b01e57bc406eca9f0776518c0450748a10724
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-15 16:58:42 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,21 +8,6 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		try {
-			new pkgs.pkg8.pkg81.ClassePKG8A().mtd();
-		} catch (Throwable t) {
-			System.out.println(t.getMessage());
-		}
-		try {
-			new pkgs.pkg8.pkg81.pkg811.ClassePKG8B().mtd();
-		} catch (Throwable t) {
-			System.out.println(t.getMessage());
-		}
-		try {
-			new pkgs.pkg8.pkg81.pkg811.pkg8111.ClassePKG8C().mtd();
-		} catch (Throwable t) {
-			System.out.println(t.getMessage());
-		}
 		System.out.println("/Exe001.m1()");
 	}
 
