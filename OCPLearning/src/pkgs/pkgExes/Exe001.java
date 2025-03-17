fee1b2c755210b1e4026f756fcf36f20bc90a0c8
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-17 19:22:49 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,25 +1,5 @@
 package pkgs.pkgExes;
 
-import pkgs.pkg3.ExceptionA;
-import pkgs.pkg3.ExceptionB;
-import pkgs.pkg3.ExceptionC;
-
-class ClasseQQ {
-
-	public void throwsExceptionA() throws ExceptionA {}
-	public void throwsExceptionAExceptionB() throws ExceptionA, ExceptionB {}
-	public void throwsExceptionAExceptionBExceptionC() throws ExceptionA, ExceptionB, ExceptionC {}
-
-	public void mtd() throws Exception {
-		try {
-			throwsExceptionAExceptionBExceptionC();
-		} catch (Exception e) {
-			throw e;
-		}
-	}
-
-}
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -28,16 +8,10 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		try {
-			new ClasseQQ().mtd();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("/Exe001.m1()");
 	}
 
 }
 
 /*
-CORRECAO 2 OK pois foi adicionado no throws uma Exception pai das exceptions disparadas no corpo do try
+
 */
