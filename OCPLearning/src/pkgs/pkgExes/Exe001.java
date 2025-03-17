b9425307f3d17fda9eb8e6f6ec00a8ff59035203
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-17 19:48:07 -0400

    ERRO testando AutoCloseable

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,5 +1,23 @@
 package pkgs.pkgExes;
 
+class ClasseAutoCloseable1 implements AutoCloseable {
+	public ClasseAutoCloseable1() {System.out.println("ClasseAutoCloseable1.this()");}
+	@Override
+	public void close() throws Exception { System.out.println("ClasseAutoCloseable1.close()"); }
+}
+
+class ClasseAutoCloseable2 implements AutoCloseable {
+	public ClasseAutoCloseable2() {System.out.println("ClasseAutoCloseable2.this()");}
+	@Override
+	public void close() throws Exception { System.out.println("ClasseAutoCloseable2.close()"); }
+}
+
+class ClasseAutoCloseable3 implements AutoCloseable {
+	public ClasseAutoCloseable3() {System.out.println("ClasseAutoCloseable3.this()");}
+	@Override
+	public void close() throws Exception { System.out.println("ClasseAutoCloseable3.close()"); }
+}
+
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -8,10 +26,20 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		try (
+				ClasseAutoCloseable1 ac1 = null;
+				ac1 = new ClasseAutoCloseable1();
+				) {
+			System.out.println("try");
+		} catch (Exception e) {
+			System.out.println("[catch][e.getMessage()="+(e.getMessage())+"]");
+		} finally {
+			System.out.println("finally");
+		}
 	}
 
 }
 
 /*
-
+ERRO testando AutoCloseable , pois NAO permite declarar o obj em uma linha e instanciar em outra
 */
