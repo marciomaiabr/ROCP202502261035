3fb6e3f57fba167e1ce10805e02fb97bde702dfc
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-18 07:18:42 -0400

    NAO e permitido try com parenteses vazio

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -9,7 +9,7 @@ class ClasseAutoCloseable1 implements AutoCloseable {
 class ClasseAutoCloseable2 implements AutoCloseable {
 	public ClasseAutoCloseable2() {System.out.println("ClasseAutoCloseable2.this()");}
 	@Override
-	public void close() throws Exception { System.out.println("ClasseAutoCloseable2.close()"); throw new RuntimeException("RTE do ClasseAutoCloseable2.close()"); }
+	public void close()  { System.out.println("ClasseAutoCloseable2.close()"); throw new RuntimeException("RTE do ClasseAutoCloseable2.close()"); }
 }
 
 class ClasseAutoCloseable3 implements AutoCloseable {
@@ -26,20 +26,8 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		int x = 5;
-		try (
-				ClasseAutoCloseable1 ac1 = new ClasseAutoCloseable1();
-				ClasseAutoCloseable2 ac2 = new ClasseAutoCloseable2();
-				x = x+1;
-				ClasseAutoCloseable3 ac3 = new ClasseAutoCloseable3();
-				) {
-			System.out.println("try");
-			System.out.println(x);
-			throw new RuntimeException("RTE do try");
-		} catch (Exception e) {
-			System.out.println("[catch][e.getMessage()="+(e.getMessage())+"]");
-		} finally {
-			System.out.println("finally");
+		try()  {
+
 		}
 	}
 
