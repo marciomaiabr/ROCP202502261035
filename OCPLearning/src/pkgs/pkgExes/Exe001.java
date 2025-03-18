cbca7ad3cee0d0ace355a8137bf7ec95fef50192
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-18 07:25:03 -0400

    testando sequencia de criacao(que e conforme vai aparecendo) e fechamento do recurso(que e diretamente inversa a de criacao)

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -3,19 +3,19 @@ package pkgs.pkgExes;
 class ClasseAutoCloseable1 implements AutoCloseable {
 	public ClasseAutoCloseable1() {System.out.println("ClasseAutoCloseable1.this()");}
 	@Override
-	public void close() throws Exception { System.out.println("ClasseAutoCloseable1.close()"); }
+	public void close() { System.out.println("ClasseAutoCloseable1.close()"); }
 }
 
 class ClasseAutoCloseable2 implements AutoCloseable {
 	public ClasseAutoCloseable2() {System.out.println("ClasseAutoCloseable2.this()");}
 	@Override
-	public void close()  { System.out.println("ClasseAutoCloseable2.close()"); throw new RuntimeException("RTE do ClasseAutoCloseable2.close()"); }
+	public void close() { System.out.println("ClasseAutoCloseable2.close()"); }
 }
 
 class ClasseAutoCloseable3 implements AutoCloseable {
 	public ClasseAutoCloseable3() {System.out.println("ClasseAutoCloseable3.this()");}
 	@Override
-	public void close() throws Exception { System.out.println("ClasseAutoCloseable3.close()"); }
+	public void close() { System.out.println("ClasseAutoCloseable3.close()"); }
 }
 
 public class Exe001 {
@@ -26,7 +26,11 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		try()  {
+		try(
+				ClasseAutoCloseable1 cac1 = new ClasseAutoCloseable1();
+				ClasseAutoCloseable2 cac2 = new ClasseAutoCloseable2();
+				ClasseAutoCloseable3 cac3 = new ClasseAutoCloseable3();
+				)  {
 
 		}
 	}
