be6839e3458134f41294a43af6f7a3871ea7bb7b
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-19 07:11:13 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,23 +1,5 @@
 package pkgs.pkgExes;
 
-class ClasseAutoCloseable1 implements AutoCloseable {
-	public ClasseAutoCloseable1() {System.out.println("ClasseAutoCloseable1.this()");}
-	@Override
-	public void close() throws Exception { System.out.println("ClasseAutoCloseable1.close()"); }
-}
-
-class ClasseAutoCloseable2 implements AutoCloseable {
-	public ClasseAutoCloseable2() {System.out.println("ClasseAutoCloseable2.this()");}
-	@Override
-	public void close() throws Exception { System.out.println("ClasseAutoCloseable2.close()"); throw new RuntimeException("RTE do ClasseAutoCloseable2.close()"); }
-}
-
-class ClasseAutoCloseable3 implements AutoCloseable {
-	public ClasseAutoCloseable3() {System.out.println("ClasseAutoCloseable3.this()");}
-	@Override
-	public void close() throws Exception { System.out.println("ClasseAutoCloseable3.close()"); }
-}
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -26,19 +8,6 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
-		try(
-				ClasseAutoCloseable1 cac1 = new ClasseAutoCloseable1();
-				ClasseAutoCloseable2 cac2 = new ClasseAutoCloseable2();
-				ClasseAutoCloseable3 cac3 = new ClasseAutoCloseable3(); 
-				)  {
-			System.out.println("Exe001.m1()[try]");
-			throw new RuntimeException("RTE do try");
-		} catch (Exception e) {
-			System.out.println("Exe001.m1()[catch][e.getMessage()="+(e.getMessage())+"]");
-			e.printStackTrace();
-		} finally {
-			System.out.println("Exe001.m1()[finally]");
-		}
 	}
 
 }
