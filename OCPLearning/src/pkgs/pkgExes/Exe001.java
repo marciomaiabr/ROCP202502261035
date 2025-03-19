412479a7c2297dbed321f676657e26500c9715b8
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-19 07:35:33 -0400

    OK try-with-resources funcionando perfeitamente com Closeable

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,5 +1,16 @@
 package pkgs.pkgExes;
 
+import java.io.Closeable;
+
+class ClasseCloseable1 implements Closeable {
+	public ClasseCloseable1() {System.out.println("ClasseCloseable1.this()");}
+
+	@Override
+	public void close() {
+		System.out.println("ClasseCloseable1.close()");
+	}
+}
+
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -8,10 +19,15 @@ public class Exe001 {
 
 	private static void m1() {
 		System.out.println("Exe001.m1()");
+		try(
+				ClasseCloseable1 cc1 = new ClasseCloseable1();
+				)  {
+			System.out.println("Exe001.m1()[try]");
+		}
 	}
 
 }
 
 /*
-
+OK try-with-resources funcionando perfeitamente com Closeable pois a interface Closeable extends da interface AutoCloseable
 */
