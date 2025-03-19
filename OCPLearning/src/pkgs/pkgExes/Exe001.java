5c0b98b1422afb08f1345378892b3a285efc3206
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-19 07:51:50 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,36 +1,17 @@
 package pkgs.pkgExes;
 
-import java.io.Closeable;
-import java.io.IOException;
-
-class ClasseCloseable1 implements Closeable {
-	public ClasseCloseable1() {System.out.println("ClasseCloseable1.this()");}
-
-	@Override
-	public void close() throws IOException {
-		System.out.println("ClasseCloseable1.close()");
-	}
-}
-
 public class Exe001 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		m1();
 	}
 
-	private static void m1() throws IOException {
+	private static void m1() {
 		System.out.println("Exe001.m1()");
-		try(
-				ClasseCloseable1 cc1 = new ClasseCloseable1();
-				)  {
-			System.out.println("Exe001.m1()[try]");
-		}
 	}
 
 }
 
 /*
-OK try-with-resources funcionando perfeitamente com Closeable pois a interface Closeable extends da interface AutoCloseable
-POREM a assinatura de close() de AutoCloseable throws Exception
-ENQUANTO a assinatura de close() de Closeable throws IOException OBRIGANDO assim um tratamento ou com bloco catch (Exception e) ou throws IOException(ou supertypes destes)
+
 */
