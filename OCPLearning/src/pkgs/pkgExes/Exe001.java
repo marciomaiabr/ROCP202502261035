06c70214906b558ffcc288a5a646fc29d27c259f
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-19 08:08:00 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,28 +1,5 @@
 package pkgs.pkgExes;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.sql.SQLException;
-
-interface ICloseable extends Closeable {
-	@Override
-	void close() throws IOException;
-}
-
-interface IAutoCloseable extends AutoCloseable {
-	@Override
-	void close() throws SQLException;
-}
-
-class Classe1 implements IAutoCloseable, ICloseable {
-
-	@Override
-	public void close() throws IOException {
-		// TODO Auto-generated method stub
-	}
-
-}
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -36,5 +13,5 @@ public class Exe001 {
 }
 
 /*
-OBVIO, basico de interfaces, que QUANDO sub de subtype com diferentes throws NAO parentescos do mesmo metodo NAO COMPILA
+
 */
