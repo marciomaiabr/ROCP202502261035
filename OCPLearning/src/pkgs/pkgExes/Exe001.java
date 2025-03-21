8935e143bcdb243376deca20add7f15c05272f1c
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-21 08:49:33 -0400

    testing time zone and daylight savings time with ZoneId

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -7,11 +7,35 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 		m1();
+		m2();
+	}
+
+	public static void m2() {
+		System.out.println("Exe001.m2()");
+		ZoneId zone = ZoneId.of("GMT-7");
+		System.out.println(zone);
+		ZonedDateTime zdt = ZonedDateTime.of(2024, 4, 1, 18, 35, 59, 9999, ZoneId.of("US/Pacific"));
+		System.out.println(zdt);
+		Instant instant = zdt.toInstant();
+		System.out.println(instant);
+		System.out.println(zone.getRules().isDaylightSavings(instant));
+		System.out.println(zone.getRules());
+		System.out.println(zone.getRules().isFixedOffset());
+		//zone.getRules().getTransitions().forEach(System.out::println);//historico de transicoes
 	}
 
 	public static void m1() {
 		System.out.println("Exe001.m1()");
-		ZoneId.getAvailableZoneIds().forEach(System.out::println);
+		ZoneId zone = ZoneId.of("US/Pacific");
+		System.out.println(zone);
+		ZonedDateTime zdt = ZonedDateTime.of(2024, 4, 1, 18, 35, 59, 9999, ZoneId.of("US/Pacific"));
+		System.out.println(zdt);
+		Instant instant = zdt.toInstant();
+		System.out.println(instant);
+		System.out.println(zone.getRules().isDaylightSavings(instant));
+		System.out.println(zone.getRules());
+		System.out.println(zone.getRules().isFixedOffset());
+		//zone.getRules().getTransitions().forEach(System.out::println);//historico de transicoes
 	}
 
 }
