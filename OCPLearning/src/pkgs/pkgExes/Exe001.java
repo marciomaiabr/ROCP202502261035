7c97866f8abd61115b4b5380b7c544420ff2369d
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-21 06:36:10 -0400

    LocalDate, LocalTime e LocalDateTime change value

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -6,34 +6,31 @@ import java.time.format.*;
 public class Exe001 {
 
 	public static void main(String[] args) {
-		m3();
+		m1();
 	}
 
 	public static void m3() {
 		System.out.println("Exe001.m3()");
 		LocalTime lt = LocalTime.of(18, 35);
 		System.out.println(lt);
-		LocalTime lt2 = lt.plusMinutes(30);
+		lt = lt.plusMinutes(30);
 		System.out.println(lt);
-		System.out.println(lt2);
 	}
 
 	public static void m2() {
 		System.out.println("Exe001.m2()");
 		LocalDate ld = LocalDate.of(2024, 10, 17);
 		System.out.println(ld);
-		LocalDate ld2 = ld.plusDays(1);
+		ld = ld.plusDays(1);
 		System.out.println(ld);
-		System.out.println(ld2);
 	}
 
 	public static void m1() {
 		System.out.println("Exe001.m1()");
 		LocalDateTime ldt = LocalDateTime.of(2024, 10, 17, 18, 35);
 		System.out.println(ldt);
-		LocalDateTime ldt2 = ldt.plusHours(1);
+		ldt = ldt.plusHours(1);
 		System.out.println(ldt);
-		System.out.println(ldt2);
 	}
 
 }
