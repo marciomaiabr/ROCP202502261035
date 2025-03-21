3f03cfe9c2aff2b4b7043f4e9b71aa3b95e25757
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-21 06:43:26 -0400

    using ZonedDateTime

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -9,28 +9,10 @@ public class Exe001 {
 		m1();
 	}
 
-	public static void m3() {
-		System.out.println("Exe001.m3()");
-		LocalTime lt = LocalTime.of(18, 35);
-		System.out.println(lt);
-		lt = lt.plusMinutes(30);
-		System.out.println(lt);
-	}
-
-	public static void m2() {
-		System.out.println("Exe001.m2()");
-		LocalDate ld = LocalDate.of(2024, 10, 17);
-		System.out.println(ld);
-		ld = ld.plusDays(1);
-		System.out.println(ld);
-	}
-
 	public static void m1() {
 		System.out.println("Exe001.m1()");
-		LocalDateTime ldt = LocalDateTime.of(2024, 10, 17, 18, 35);
-		System.out.println(ldt);
-		ldt = ldt.plusHours(1);
-		System.out.println(ldt);
+		ZonedDateTime zdt = ZonedDateTime.of(2024, 10, 17, 18, 35, 59, 9999, ZoneId.systemDefault());
+		System.out.println(zdt);
 	}
 
 }
