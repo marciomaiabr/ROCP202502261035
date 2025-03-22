78cb82c6e9fc7bf8d1befa9da123f4ddb0f3ff0d
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-22 12:40:07 -0400

    tst mtds withZoneSameInstant and withZoneSameLocal 4 ZonedDateTime class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -12,35 +12,102 @@ public class Exe001 {
 
 	public static void m1() {
 		System.out.println("Exe001.m1()");
-		LocalDate ld = LocalDate.of(2024, 4, 8);
-		LocalTime lt = LocalTime.of(13, 35, 56, 0);
+		LocalDate ld = LocalDate.of(2024, 4, 10);
+		LocalTime lt = LocalTime.of(2, 30, 0, 0);
 		LocalDateTime ldt = LocalDateTime.of(ld, lt);
 		System.out.println(ldt);
-		ZoneId zoneIdUSCentral = ZoneId.of("US/Central");
-		ZonedDateTime zdt = ZonedDateTime.of(ldt, zoneIdUSCentral);
+
+		System.out.println();
+
+		ZoneId zoneId = ZoneId.of("US/Central");
+
+		ZonedDateTime zdt = ZonedDateTime.of(ldt, zoneId);
 		System.out.println(zdt);
-		Instant instant = zdt.toInstant();
-		System.out.println("[instant="+(instant)+"]");
-		System.out.println("[ZonedDateTime.ofInstant(instant, zoneIdUSCentral)="+(ZonedDateTime.ofInstant(instant, zoneIdUSCentral))+"]");
-		System.out.println("[ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())="+(ZonedDateTime.ofInstant(instant, ZoneId.systemDefault()))+"]");
+		System.out.println("[isDaylightSavings="+(zdt.getZone().getRules().isDaylightSavings(zdt.toInstant()))+"]");
+
+		System.out.println();
+
+		System.out.println("Um mês atrás, primeiro dia horário de verão");
+
+		System.out.println();
+
+		ZonedDateTime zdt2 = zdt.minus(Period.ofMonths(1));
+		System.out.println(zdt2);
+		System.out.println("[isDaylightSavings="+(zdt2.getZone().getRules().isDaylightSavings(zdt2.toInstant()))+"]");
+
+		System.out.println();
+
+		System.out.println("mudando o zoneId de US/Central para US/Pacific");
+
+		System.out.println();
+
+		zoneId = ZoneId.of("US/Pacific");
+
+		System.out.println("withZoneSameInstant");
+		ZonedDateTime zdt3 = zdt2.withZoneSameInstant(zoneId);//sendo Instant o LocalDateTime corrigido para o GMT Z, ele pega esse valor e corrige para o zoneId do parâmetro
+		System.out.println(zdt3);
+		System.out.println("[isDaylightSavings="+(zdt3.getZone().getRules().isDaylightSavings(zdt3.toInstant()))+"]");
+
+		System.out.println();
+
+		System.out.println("withZoneSameLocal");
+		ZonedDateTime zdt4 = zdt2.withZoneSameLocal(zoneId);//ele TENTA manter o LocalDateTime, porém há situações em que não consegue, vide abaixo
+		System.out.println(zdt4);
+		System.out.println("[isDaylightSavings="+(zdt4.getZone().getRules().isDaylightSavings(zdt4.toInstant()))+"]");
 	}
 
 }
 
 /*
+
+neste caso withZoneSameLocal MANTÉM O MESMO LocalDateTime
+
 <>
 Exe001.m1()
-2024-04-08T13:35:56
-2024-04-08T13:35:56-05:00[US/Central]
-[instant=2024-04-08T18:35:56Z]
-[ZonedDateTime.ofInstant(instant, zoneIdUSCentral)=2024-04-08T13:35:56-05:00[US/Central]]
-[ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())=2024-04-08T14:35:56-04:00[America/Cuiaba]]
-</>
-*/
+2024-04-10T05:30
 
-/*
-testando Instant
-comparando com timestamps é como se fosse um gigante long, não é timestamps pois Instant inclui também nanoseconds e ficaria muito muito grande
-para OBTER um Instant PRECISA de um ZoneId
-POIS Instant nada mais é do que o ZonedDateTime calculado no GMT raíz/central/inicial/0 que é indicado com o "Z" no final ao invés de "US/Central" por exemplo
+2024-04-10T05:30-05:00[US/Central]
+[isDaylightSavings=true]
+
+Um mês atrás, primeiro dia horário de verão
+
+2024-03-10T05:30-05:00[US/Central]
+[isDaylightSavings=true]
+
+mudando o zoneId de US/Central para US/Pacific
+
+withZoneSameInstant
+2024-03-10T03:30-07:00[US/Pacific]
+[isDaylightSavings=true]
+
+withZoneSameLocal
+2024-03-10T05:30-07:00[US/Pacific]
+[isDaylightSavings=true]
+</>
+
+neste caso withZoneSameLocal TENTA MANTER O MESMO LocalDateTime PORÉM PRIMEIRO DIA DE Daylight portanto NÃO EXISTE T02:30 POIS ADIANTA-SE 1H, ou seja T03:30
+
+<>
+Exe001.m1()
+2024-04-10T02:30
+
+2024-04-10T02:30-05:00[US/Central]
+[isDaylightSavings=true]
+
+Um mês atrás, primeiro dia horário de verão
+
+2024-03-10T03:30-05:00[US/Central]
+[isDaylightSavings=true]
+
+mudando o zoneId de US/Central para US/Pacific
+
+withZoneSameInstant
+2024-03-10T00:30-08:00[US/Pacific]
+[isDaylightSavings=false]
+
+withZoneSameLocal
+2024-03-10T03:30-07:00[US/Pacific]
+[isDaylightSavings=true]
+</>
+
 */
