77a4458935b38d68257a47e9840d8ccb74bcfd97
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-22 10:28:13 -0400

    Period

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -15,17 +15,48 @@ public class Exe001 {
 		LocalDate ld = LocalDate.of(2024, 4, 8);
 		LocalTime lt = LocalTime.of(13, 35, 56, 0);
 		LocalDateTime ldt = LocalDateTime.of(ld, lt);
-		ZoneId zoneId = ZoneId.of("US/Central");
-		ZonedDateTime zdt = ZonedDateTime.of(ldt, zoneId);
+		System.out.println(ldt);
+
+		System.out.println();
+
+		ZoneId zoneIdUSCentral = ZoneId.of("US/Central");
+
+		ZonedDateTime zdt = ZonedDateTime.of(ldt, zoneIdUSCentral);
 		System.out.println(zdt);
+		System.out.println(zdt.toLocalDateTime());
+		System.out.println("[isDaylightSavings="+(zdt.getZone().getRules().isDaylightSavings(zdt.toInstant()))+"]");
+
+		System.out.println();
+
+		Period period = Period.ofMonths(1);
+		System.out.println(period);
+
+		ZonedDateTime zdt2 = zdt.minus(period);
+		System.out.println(zdt2);
+		LocalDateTime ldt2 = zdt2.toLocalDateTime();
+		System.out.println(ldt2);
+		System.out.println("[isDaylightSavings="+(zdt2.getZone().getRules().isDaylightSavings(zdt2.toInstant()))+"]");
 	}
 
 }
 
 /*
+<>
+Exe001.m1()
+2024-04-08T13:35:56
 
+2024-04-08T13:35:56-05:00[US/Central]
+2024-04-08T13:35:56
+[isDaylightSavings=true]
+
+P1M
+2024-03-08T13:35:56-06:00[US/Central]
+2024-03-08T13:35:56
+[isDaylightSavings=false]
+</>
 */
 
 /*
-
+testando Period
+período de dias, meses ou anos
 */
