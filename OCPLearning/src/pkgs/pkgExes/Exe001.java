826fce5c8613ee7df0c58695a03b635ae2a6a0bd
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-22 11:57:31 -0400

    Instant

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,7 +1,6 @@
 package pkgs.pkgExes;
 
 import java.time.*;
-import java.time.temporal.ChronoUnit;
 
 public class Exe001 {
 
@@ -13,25 +12,17 @@ public class Exe001 {
 
 	public static void m1() {
 		System.out.println("Exe001.m1()");
-		LocalTime lt1 = LocalTime.of(12, 17, 32);
-		System.out.println(lt1);
-		LocalTime lt2 = LocalTime.of(13, 35, 56);
-		System.out.println(lt2);
-		long minutos = ChronoUnit.MINUTES.between(lt1, lt2);//perda de segundos na conversão
-		System.out.println("[minutos="+(minutos)+"]");
-		Duration duration = Duration.ofMinutes(minutos);
-		System.out.println("[duration="+(duration)+"]");
-		LocalTime lt3 = lt1.plus(duration);
-		System.out.println("[lt2="+(lt2)+"][lt3="+(lt3)+"]");//não bateu pois houve perda de segundos na conversão
-
-		System.out.println();
-
-		System.out.println("[Duration.ofHours(49)="+(Duration.ofHours(49))+"][Duration.ofDays(3)="+(Duration.ofDays(3))+"]");//não são exibidos como dias para evitar confusão com Period
-
-		System.out.println("[Duration.of(5, ChronoUnit.DAYS)="+(Duration.of(5, ChronoUnit.DAYS))+"]");
-
-		//System.out.println("[Duration.of(5, ChronoUnit.MONTHS)="+(Duration.of(5, ChronoUnit.MONTHS))+"]");//maior ChronoUnit permitido é DAYS//UnsupportedTemporalTypeException: Unit must not have an estimated duration
-
+		LocalDate ld = LocalDate.of(2024, 4, 8);
+		LocalTime lt = LocalTime.of(13, 35, 56, 0);
+		LocalDateTime ldt = LocalDateTime.of(ld, lt);
+		System.out.println(ldt);
+		ZoneId zoneIdUSCentral = ZoneId.of("US/Central");
+		ZonedDateTime zdt = ZonedDateTime.of(ldt, zoneIdUSCentral);
+		System.out.println(zdt);
+		Instant instant = zdt.toInstant();
+		System.out.println("[instant="+(instant)+"]");
+		System.out.println("[ZonedDateTime.ofInstant(instant, zoneIdUSCentral)="+(ZonedDateTime.ofInstant(instant, zoneIdUSCentral))+"]");
+		System.out.println("[ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())="+(ZonedDateTime.ofInstant(instant, ZoneId.systemDefault()))+"]");
 	}
 
 }
@@ -39,18 +30,17 @@ public class Exe001 {
 /*
 <>
 Exe001.m1()
-12:17:32
-13:35:56
-[minutos=78]
-[duration=PT1H18M]
-[lt2=13:35:56][lt3=13:35:32]
-
-[Duration.ofHours(49)=PT49H][Duration.ofDays(3)=PT72H]
-[Duration.of(5, ChronoUnit.DAYS)=PT120H]
+2024-04-08T13:35:56
+2024-04-08T13:35:56-05:00[US/Central]
+[instant=2024-04-08T18:35:56Z]
+[ZonedDateTime.ofInstant(instant, zoneIdUSCentral)=2024-04-08T13:35:56-05:00[US/Central]]
+[ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())=2024-04-08T14:35:56-04:00[America/Cuiaba]]
 </>
 */
 
 /*
-testando Duration
-horas, minutos e segundos
+testando Instant
+comparando com timestamps é como se fosse um gigante long, não é timestamps pois Instant inclui também nanoseconds e ficaria muito muito grande
+para OBTER um Instant PRECISA de um ZoneId
+POIS Instant nada mais é do que o ZonedDateTime calculado no GMT raíz/central/inicial/0 que é indicado com o "Z" no final ao invés de "US/Central" por exemplo
 */
