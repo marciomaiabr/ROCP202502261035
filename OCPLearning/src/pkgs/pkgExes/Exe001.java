e59472c0803fcf0703876edc2bed8ae85531a406
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-22 08:29:07 -0400

    testing TemporalAdjusters

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -2,6 +2,7 @@ package pkgs.pkgExes;
 
 import java.time.*;
 import java.time.format.*;
+import java.time.temporal.TemporalAdjusters;
 
 public class Exe001 {
 
@@ -11,6 +12,11 @@ public class Exe001 {
 
 	public static void m1() {
 		System.out.println("Exe001.m1()");
+		ZonedDateTime zdt = ZonedDateTime.of(2024, 4, 1, 18, 35, 59, 9999, ZoneId.systemDefault());
+		System.out.println(zdt);
+		ZonedDateTime zdt2 = zdt.with(TemporalAdjusters.next(DayOfWeek.WEDNESDAY));
+		System.out.println(zdt);
+		System.out.println(zdt2);
 	}
 
 }
