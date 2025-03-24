3f102ece711f5dae0a0a33d50b3d42a366670c35
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-24 11:40:43 -0400

    ResourceBundle.getBundle with locale default

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -18,13 +18,8 @@ public class Exe001 {
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
 		System.out.println("[Locale.getDefault()="+(Locale.getDefault())+"]");
-		String args0 = args.length > 0 ? args[0] : null;
-		System.out.println("[args0="+(args0)+"]");
-		Locale locale = args0 != null ? new Locale(args0) : Locale.getDefault();
-		System.out.println("[locale="+(locale)+"]");
-		ResourceBundle resourceBundle = ResourceBundle.getBundle("pkgs.pkg9.MmProperties", locale);
+		ResourceBundle resourceBundle = ResourceBundle.getBundle("MmProperties");
 		System.out.println("[resourceBundle.getString(mmProperty)="+(resourceBundle.getString("mmProperty"))+"]");
-		System.out.println("[resourceBundle.getObject(mmProperty)="+(resourceBundle.getObject("mmProperty"))+"]");
 	}
 
 }
@@ -33,13 +28,10 @@ public class Exe001 {
 <>
 Exe001.m1()
 [Locale.getDefault()=pt_BR]
-[args0=null]
-[locale=pt_BR]
 [resourceBundle.getString(mmProperty)=mmPropertyValue do MmProperties_pt_BR.java]
-[resourceBundle.getObject(mmProperty)=mmPropertyValue do MmProperties_pt_BR.java]
 </>
 */
 
 /*
-quando trabalhar com ListResourceBundle no ResourceBundle.getBundle deve-se especificar o nome COMPLETO
+usando o default
 */
