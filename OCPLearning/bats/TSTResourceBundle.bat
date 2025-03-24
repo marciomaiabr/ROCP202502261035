f7fe6e330532b45229a7096bb3766155bca25419
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-24 08:17:52 -0400

    run testing ResourceBundle and Locale classes

---
+++ OCPLearning/bats/TSTResourceBundle.bat
@@ -0,0 +1,6 @@
+cls
+rmdir /s /q "C:\temp\mmbin"
+mkdir "C:\temp\mmbin"
+javac -d "C:/temp/mmbin" -cp .;..;../src/;../lib/; ../src/pkgs/pkgExes/Exe001.java
+java -cp .;..;C:/temp/mmbin;../lib/; pkgs.pkgExes.Exe001 pt
+pause
