5a0cc5f78fd02f05497d315ab8795f9f4bc1a6ab
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-24 09:18:50 -0400

    handling exceptions

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -18,9 +18,9 @@ public class Exe001 {
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
 		System.out.println("[Locale.getDefault()="+(Locale.getDefault())+"]");
-		String args0 = args[0];
+		String args0 = args.length > 0 ? args[0] : null;
 		System.out.println("[args0="+(args0)+"]");
-		Locale locale = new Locale(args0);
+		Locale locale = args0 != null ? new Locale(args0) : Locale.getDefault();
 		System.out.println("[locale="+(locale)+"]");
 		ResourceBundle resourceBundle = ResourceBundle.getBundle("mmProperties", locale);
 		String sMmProperty = resourceBundle.getString("mmProperty");
@@ -30,17 +30,9 @@ public class Exe001 {
 }
 
 /*
-<>
-Exe001.m1()
-[Locale.getDefault()=ja_JP]
-[args0=qqc]
-[locale=qqc]
-[sMmProperty=mmPropertyValue do mmProperties_ja_JP.properties]
-</>
+
 */
 
 /*
-using line command
--Duser.language=
--Duser.region=JP
+
 */
