51f9c7e05d2faee0af398abd36df5b034e3ba08f
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-24 11:11:06 -0400

    corrigindo bat

--- OCPLearning/bats/EclipseBat_TSTResourceBundle.bat
+++ OCPLearning/bats/EclipseBat_TSTResourceBundle.bat
@@ -1,7 +1,1 @@
-cd C:\Users\Administrador\git\ROCP202502261035\OCPLearning\bats\
-::cls
-::rmdir /s /q "C:\temp\mmbin"
-::mkdir "C:\temp\mmbin"
-::javac -d "C:/temp/mmbin" -cp .;..;../src/;../lib/; ../src/pkgs/pkgExes/Exe001.java
-java -cp .;..;C:/temp/mmbin;../lib/; pkgs.pkgExes.Exe001
-::pause
+java -cp .;..;../bin/;../lib/; pkgs.pkgExes.Exe001
