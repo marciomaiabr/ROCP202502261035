122736903ee8748a858ea88b716a216619182d2d
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-25 09:08:12 -0400

    cleanning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,12 +1,5 @@
 package pkgs.pkgExes;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.ResourceBundle;
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -17,22 +10,12 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		System.out.println("[Locale.getDefault()="+(Locale.getDefault())+"]");
-		Locale.setDefault(new Locale("ja", "JP"));
-		System.out.println("[Locale.getDefault()="+(Locale.getDefault())+"]");
-		ResourceBundle resourceBundle = ResourceBundle.getBundle("MmProperties", new Locale("fr", "FR"));
-		System.out.println("[resourceBundle.getString(mmProperty)="+(resourceBundle.getString("mmProperty"))+"]");
 	}
 
 }
 
 /*
-<>
-Exe001.m1()
-[Locale.getDefault()=pt_BR]
-[Locale.getDefault()=ja_JP]
-[resourceBundle.getString(mmProperty)=mmPropertyValue do MmProperties_ja_JP.java]
-</>
+
 */
 
 /*
