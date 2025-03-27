fa4757bb036983858e595fab82ce882f339f50f6
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-27 08:47:47 -0400

    FileInputStream

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,7 @@
 package pkgs.pkgExes;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class Exe001 {
@@ -14,12 +15,23 @@ public class Exe001 {
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
 		try {
+			byte [] in = new byte[20];
+			int size = 0;
 			File file = new File("C:\\temp\\fileWrite3.txt");
-			FileOutputStream fileOutputStream = new FileOutputStream(file);
-			fileOutputStream.write("linha1\nlinha2\n".getBytes());
-			fileOutputStream.flush();
-			fileOutputStream.write("linha3\nlinha4\n".getBytes());
-			fileOutputStream.close();
+			FileInputStream fileInputStream = new FileInputStream(file);
+			size = fileInputStream.read(in);
+			System.out.println("[size="+(size)+"]");
+
+			for (byte b : in)
+				System.out.print("[b="+(b)+"]");
+
+			System.out.println();
+			System.out.println();
+
+			for (byte b : in)
+				System.out.print("[(char)b="+((char)b)+"]");
+
+			fileInputStream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -31,14 +43,17 @@ public class Exe001 {
 C:\temp\fileWrite3.txt
 linha1
 linha2
-linha3
-linha4
 */
 
 /*
 <>
 Exe001.m1()
-</>
+[size=14]
+[b=108][b=105][b=110][b=104][b=97][b=49][b=10][b=108][b=105][b=110][b=104][b=97][b=50][b=10][b=0][b=0][b=0][b=0][b=0][b=0]
+
+[(char)b=l][(char)b=i][(char)b=n][(char)b=h][(char)b=a][(char)b=1][(char)b=
+][(char)b=l][(char)b=i][(char)b=n][(char)b=h][(char)b=a][(char)b=2][(char)b=
+][(char)b= ][(char)b= ][(char)b= ][(char)b= ][(char)b= ][(char)b= ]</>
 */
 
 /*
