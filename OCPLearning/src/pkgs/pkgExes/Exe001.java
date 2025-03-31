5f55823397b92159e9d2842ffa433563339346e7
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-03-31 19:23:37 -0400

    File

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,26 +1,37 @@
 package pkgs.pkgExes;
 
 import java.io.File;
-import java.io.FileWriter;
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 		System.out.println("<>");
-		m1(args);
+		m2(args);
 		System.out.println("</>");
+	}
+
+	public static void m2(String[] args) {
+		System.out.println("Exe001.m2()");
+		//cria diretorio em estrutura existente
+		try {
+			File temp = new File("C:\\temp");
+			File dir1 = new File(temp, "dir1");
+			File dir11 = new File(dir1, "dir11");
+			File dir111 = new File(dir11, "dir111");
+			System.out.println("[dir1.mkdir()="+(dir111.mkdir())+"]");//não cria pois a estrutura não existe
+			System.out.println("[dir1.mkdir()="+(dir111.mkdirs())+"]");//cria a estrutura completa
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
+		//cria diretorio em estrutura existente
 		try {
-			File file = new File("C:\\temp\\File202503310853.txt");
-			System.out.println("[file.exists()="+(file.exists())+"]");
-			FileWriter fileWriter = new FileWriter(file);
-			fileWriter.write("linha1\nlinha2");
-			fileWriter.flush();
-			System.out.println("[file.exists()="+(file.exists())+"]");
-			fileWriter.close();
+			File temp = new File("C:\\temp");
+			File dir1 = new File(temp, "dir1");
+			System.out.println("[dir1.mkdir()="+(dir1.mkdir())+"]");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -29,17 +40,13 @@ public class Exe001 {
 }
 
 /*
-<>
-Exe001.m1()
-[file.exists()=false]
-[file.exists()=true]
-</>
-*/
-
-/*
 
 */
 
 /*
-flush() força a criação do arquivo
+
+*/
+
+/*
+
 */
