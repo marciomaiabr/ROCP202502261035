22b2ee33907196d0884b9f59b6e83c8c6da8fded
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-04-02 07:40:40 -0400

    Path normalize

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -13,26 +13,25 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		Path path1 = Paths.get("src/main/java");
-		Path path2 = Paths.get("src/resources/images");
-
-		Path relativePath = path1.relativize(path2);
-		System.out.println("Relative Path: " + relativePath);
+		Path path = Paths.get("/folderA/folderAA", "../..", "folderB/folderBB");
+		System.out.println("[path="+(path)+"]");
+		System.out.println("[path.normalize()="+(path.normalize())+"]");
 	}
 
 }
 
 /*
-
-*/
-
-/*
 <>
 Exe001.m1()
-Relative Path: ..\..\resources\images
+[path=\folderA\folderAA\..\..\folderB\folderBB]
+[path.normalize()=\folderB\folderBB]
 </>
 */
 
 /*
-https://medium.com/@AlexanderObregon/javas-path-relativize-method-explained-773c3dd6e181
+
+*/
+
+/*
+
 */
