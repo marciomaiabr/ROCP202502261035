473d4d8e692d12dfa0b2b3f8bf6aadc101d84b48
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-04-04 08:03:42 -0400

    Integer and Long comparation

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,9 +1,5 @@
 package pkgs.pkgExes;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -13,15 +9,44 @@ public class Exe001 {
 	}
 
 	public static void m1(String[] args) {
-		System.out.println("Exe001.m1()");
-		System.out.println("[="+(LocalDate.now().format(DateTimeFormatter.ofPattern("EEEE", new Locale("PT"))))+"]");
-		System.out.println("[="+(LocalDate.now().format(DateTimeFormatter.ofPattern("EEEE", new Locale("EN"))))+"]");
+		System.out.println("Exe001.m1()");//https://stackoverflow.com/questions/1700081/why-is-128-128-false-but-127-127-is-true-when-comparing-integer-wrappers-in-ja
+
+		{
+			Integer i1 = 127;
+			Integer i2 = 127;
+			System.out.println("["+(i1==i2)+"]");
+		}
+
+		{
+			Integer i1 = 128;
+			Integer i2 = 128;
+			System.out.println("["+(i1==i2)+"]");
+		}
+
+		{
+			Long l1 = 127l;
+			Long l2 = 127l;
+			System.out.println("["+(l1==l2)+"]");
+		}
+
+		{
+			Long l1 = 128l;
+			Long l2 = 128l;
+			System.out.println("["+(l1==l2)+"]");
+		}
+
 	}
 
 }
 
 /*
-
+<>
+Exe001.m1()
+[true]
+[false]
+[true]
+[false]
+</>
 */
 
 /*
