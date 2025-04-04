b1b767bf43d58837d77cc22986f3d0e9c187463e
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-04-04 10:48:22 -0400

    DirectoryStream glob pattern filter

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -18,14 +18,19 @@ public class Exe001 {
 		try {
 			Path root = Paths.get("c:", "temp");
 			if(Files.notExists(root.resolve(Paths.get("folderB","folderBB","202504040905.txt")))) {
+				Files.createFile(root.resolve("R.txt"));
+				Files.createFile(root.resolve("K.txt"));
+				Files.createFile(root.resolve("w.txt"));
+				Files.createFile(root.resolve("Y.txt"));
+				Files.createFile(root.resolve("z.txt"));
 				Files.createDirectories(root.resolve(Paths.get("folderA","folderAA","folderAAA")));
 				Files.createDirectories(root.resolve(Paths.get("folderB","folderBB","folderBBB")));
 				Files.createDirectories(root.resolve(Paths.get("folderC","folderCC","folderCCC")));
 				Files.createFile(root.resolve(Paths.get("folderB","folderBB","202504040905.txt")));
 			}
-			try(DirectoryStream<Path> directoryStream = Files.newDirectoryStream(root)){
+			try(DirectoryStream<Path> directoryStream = Files.newDirectoryStream(root, "[rf]*")){//ou r ou f //MAIÚSCULA ou minúscula
 				for(Path path : directoryStream) {
-					System.out.println(path);
+					System.out.println(path.getFileName());
 				}
 			}
 		} catch (Exception e) {
