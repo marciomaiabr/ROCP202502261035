abea91fd66b9518d8266f55ead8200750a4f4b64
Marcio Luis Borges Maia <marcio.maia@contrato.g4f.com.br>
2025-04-10 11:23:00 -0400

    SimpleFileVisitor

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -29,7 +29,8 @@ public class Exe001 {
 				Files.createDirectories(root.resolve(Paths.get("folderA","folderAA","folderAAA")));
 				Files.createDirectories(root.resolve(Paths.get("folderB","folderBB","folderBBB")));
 				Files.createDirectories(root.resolve(Paths.get("folderC","folderCC","folderCCC")));
-				Files.createFile(root.resolve(Paths.get("folderB","folderBB","202504040905.txt")));
+				Files.createFile(root.resolve(Paths.get("folderB","folderBB","fz.txt")));
+				Files.createFile(root.resolve(Paths.get("folderB","folderBB","fa.txt")));
 			}
 			SimpleFileVisitor<Path> simpleFileVisitor = new SimpleFileVisitor<Path>() {
 				@Override
@@ -66,7 +67,57 @@ public class Exe001 {
 */
 
 /*
+<>
+Exe001.m1()
 
+
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp]
+
+
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderA]
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderA\folderAA]
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderA\folderAA\folderAAA]
+
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderA\folderAA\folderAAA][exc=null]
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderA\folderAA][exc=null]
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderA][exc=null]
+
+
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderB]
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderB\folderBB]
+
+TestSimpleFileVisitor.visitFile()[file=c:\temp\folderB\folderBB\fa.txt]
+
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderB\folderBB\folderBBB]
+
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderB\folderBB\folderBBB][exc=null]
+
+TestSimpleFileVisitor.visitFile()[file=c:\temp\folderB\folderBB\fz.txt]
+
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderB\folderBB][exc=null]
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderB][exc=null]
+
+
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderC]
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderC\folderCC]
+TestSimpleFileVisitor.preVisitDirectory()[dir=c:\temp\folderC\folderCC\folderCCC]
+
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderC\folderCC\folderCCC][exc=null]
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderC\folderCC][exc=null]
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp\folderC][exc=null]
+
+
+TestSimpleFileVisitor.visitFile()[file=c:\temp\K.txt]
+TestSimpleFileVisitor.visitFile()[file=c:\temp\R.txt]
+TestSimpleFileVisitor.visitFile()[file=c:\temp\w.txt]
+TestSimpleFileVisitor.visitFile()[file=c:\temp\Y.txt]
+TestSimpleFileVisitor.visitFile()[file=c:\temp\z.txt]
+
+
+TestSimpleFileVisitor.postVisitDirectory()[dir=c:\temp][exc=null]
+
+
+</>
 */
 
 /*
