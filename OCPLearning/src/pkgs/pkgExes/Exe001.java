697e8d203fbbfa3f54925ae0d9b363227e604c97
Administrador <Administrador@MMW10LAB.mshome.net>
2025-04-17 11:34:11 -0400

    readObject without class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -7,59 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-class Colar {
-	private int collarSize;
-	public Colar(int collarSize) {
-		this.collarSize = collarSize;
-	}
-	public int getCollarSize() {
-		return collarSize;
-	}
-}
-
-class Dog implements Serializable {
-	private transient Colar colar;
-	private int dogSize;
-	public Dog(Colar colar, int dogSize) {
-		super();
-		this.colar = colar;
-		this.dogSize = dogSize;
-	}
-	public Colar getColar() {
-		return colar;
-	}
-	public int getDogSize() {
-		return dogSize;
-	}
-	//https://docs.oracle.com/javase/7/docs/api/java/io/ObjectOutputStream.html
-	//https://docs.oracle.com/en/java/javase/11/docs/specs/serialization/input.html
-	private void writeObject(ObjectOutputStream oos) {
-		System.out.println("Dog.writeObject()");
-		try {
-			oos.defaultWriteObject();//se ñ chamar, não escreve os atributos automaticamente, como dogSize por exemplo//se chamar um sem o outro=StreamCorruptedException: invalid type code: 00
-			oos.writeInt(colar.getCollarSize());
-			oos.writeInt(100);
-			oos.writeInt(200);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	private void readObject(ObjectInputStream ois) {
-		System.out.println("Dog.readObject()");
-		try {
-			ois.defaultReadObject();//se ñ chamar, não carrega os atributos automaticamente, como dogSize por exemplo//se chamar um sem o outro=StreamCorruptedException: invalid type code: 00
-			colar = new Colar(ois.readInt());
-			System.out.println("[="+(ois.readInt())+"]");
-			System.out.println("[="+(ois.readInt())+"]");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	private void readObjectNoData() {
-		System.out.println("Dog.readObjectNoData()");
-	}
-}
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -72,22 +19,10 @@ public class Exe001 {
 		System.out.println("Exe001.m1()");
 		File file = new File("C:\\temp\\dog.ser");
 		try {
-			Colar colar = new Colar(3);
-			Dog dog = new Dog(colar, 8);
-			System.out.println(file.exists());
-			FileOutputStream fos = new FileOutputStream(file);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(dog);
-			oos.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
 			FileInputStream fos = new FileInputStream(file);
 			ObjectInputStream ois = new ObjectInputStream(fos);
-			Dog dog = (Dog) ois.readObject();
-			System.out.println(dog.getDogSize());
-			System.out.println(dog.getColar().getCollarSize());
+			Object o = ois.readObject();
+			System.out.println(o);
 			ois.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -99,14 +34,23 @@ public class Exe001 {
 /*
 <>
 Exe001.m1()
-false
-Dog.writeObject()
-Dog.readObject()
-[=100]
-[=200]
-8
-3
-</>
+java.lang.ClassNotFoundException: pkgs.pkgExes.Dog
+	at java.net.URLClassLoader.findClass(URLClassLoader.java:387)
+	at java.lang.ClassLoader.loadClass(ClassLoader.java:418)
+	at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:359)
+	at java.lang.ClassLoader.loadClass(ClassLoader.java:351)
+	at java.lang.Class.forName0(Native Method)
+	at java.lang.Class.forName(Class.java:348)
+	at java.io.ObjectInputStream.resolveClass(ObjectInputStream.java:763)
+	at java.io.ObjectInputStream.readNonProxyDesc(ObjectInputStream.java:2011)
+	at java.io.ObjectInputStream.readClassDesc(ObjectInputStream.java:1875)
+	at java.io.ObjectInputStream.readOrdinaryObject(ObjectInputStream.java:2209)
+	at java.io.ObjectInputStream.readObject0(ObjectInputStream.java:1692)
+	at java.io.ObjectInputStream.readObject(ObjectInputStream.java:508)
+	at java.io.ObjectInputStream.readObject(ObjectInputStream.java:466)</>
+
+	at pkgs.pkgExes.Exe001.m1(Exe001.java:24)
+	at pkgs.pkgExes.Exe001.main(Exe001.java:14)
 */
 
 /*
