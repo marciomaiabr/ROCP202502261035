9048373235f887d7aeee40da6b7e435b1286b473
Administrador <Administrador@MMW10LAB.mshome.net>
2025-04-17 09:31:25 -0400

    FileOutputStream ObjectOutputStream

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -7,7 +7,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-class Colar {
+class Colar implements Serializable {
 	private int collarSize;
 	public Colar(int collarSize) {
 		this.collarSize = collarSize;
@@ -64,15 +64,6 @@ public class Exe001 {
 <>
 Exe001.m1()
 false
-java.io.NotSerializableException: pkgs.pkgExes.Colar
-	at java.io.ObjectOutputStream.writeObject0(ObjectOutputStream.java:1184)
-	at java.io.ObjectOutputStream.defaultWriteFields(ObjectOutputStream.java:1548)
-	at java.io.ObjectOutputStream.writeSerialData(ObjectOutputStream.java:1509)
-	at java.io.ObjectOutputStream.writeOrdinaryObject(ObjectOutputStream.java:1432)
-	at java.io.ObjectOutputStream.writeObject0(ObjectOutputStream.java:1178)
-	at java.io.ObjectOutputStream.writeObject(ObjectOutputStream.java:348)
-	at pkgs.pkgExes.Exe001.m1(Exe001.java:53)
-	at pkgs.pkgExes.Exe001.main(Exe001.java:40)
 true
 </>
 */
