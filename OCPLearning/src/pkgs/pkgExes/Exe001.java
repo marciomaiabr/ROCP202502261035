59f664f3eaf31fcc9f5c5a65dbd6d2ff8ba23954
Administrador <Administrador@MMW10LAB.mshome.net>
2025-04-19 08:40:42 -0400

    InvalidClassException incompatible serialVersionUID

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,43 +8,39 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.nio.file.Files;
 
-
+class Pessoa implements Serializable {
+	private int attribute1;
+	private int attribute2;
+	public int getAttribute1() {
+		return attribute1;
+	}
+	public void setAttribute1(int attribute1) {
+		this.attribute1 = attribute1;
+	}
+	public int getAttribute2() {
+		return attribute2;
+	}
+	public void setAttribute2(int attribute2) {
+		this.attribute2 = attribute2;
+	}
+}
 
 public class Exe001 {
 
-	public static File file = new File("C:\\temp\\pessoa.ser");
-
 	public static void main(String[] args) {
 		System.out.println("<>");
-		m1(args);
+		m2(args);
 		System.out.println("</>");
 	}
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		new Exe002().m1();
-		new Exe003().m1();
-	}
-
-}
-
-class Exe002 implements Serializable {
-
-	public class Pessoa implements Serializable {
-		private int attribute1;
-		public int getAttribute1() {
-			return attribute1;
-		}
-		public void setAttribute1(int attribute1) {
-			this.attribute1 = attribute1;
-		}
-	}
-
-	public void m1() {
+		File file = new File("C:\\temp\\pessoa.ser");
 		try {
-			Files.deleteIfExists(Exe001.file.toPath());
+			Files.deleteIfExists(file.toPath());
 			Pessoa pessoa = new Pessoa();
-			FileOutputStream fos = new FileOutputStream(Exe001.file);
+			System.out.println(file.exists());
+			FileOutputStream fos = new FileOutputStream(file);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(pessoa);
 			oos.close();
@@ -53,28 +49,16 @@ class Exe002 implements Serializable {
 		}
 	}
 
-}
-
-class Exe003 implements Serializable {
-
-	public class Pessoa implements Serializable {
-		private int attribute1;
-		public int getAttribute1() {
-			return attribute1;
-		}
-		public void setAttribute1(int attribute1) {
-			this.attribute1 = attribute1;
-		}
-	}
-
-	public void m1() {
+	public static void m2(String[] args) {
+		System.out.println("Exe001.m2()");
+		File file = new File("C:\\temp\\pessoa.ser");
 		try {
-			FileInputStream fos = new FileInputStream(Exe001.file);
+			FileInputStream fos = new FileInputStream(file);
 			ObjectInputStream ois = new ObjectInputStream(fos);
 			Pessoa pessoa = (Pessoa) ois.readObject();
 			System.out.println(pessoa);
 			ois.close();
-			Files.deleteIfExists(Exe001.file.toPath());
+			Files.deleteIfExists(file.toPath());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -84,11 +68,17 @@ class Exe003 implements Serializable {
 
 /*
 <>
-Exe001.m1()
-java.lang.ClassCastException: pkgs.pkgExes.Exe002$Pessoa cannot be cast to pkgs.pkgExes.Exe003$Pessoa
-	at pkgs.pkgExes.Exe003.m1(Exe001.java:74)
-	at pkgs.pkgExes.Exe001.m1(Exe001.java:26)
-	at pkgs.pkgExes.Exe001.main(Exe001.java:19)
+Exe001.m2()
+java.io.InvalidClassException: pkgs.pkgExes.Pessoa; local class incompatible: stream classdesc serialVersionUID = 6924733891751361056, local class serialVersionUID = -7354908922431995500
+	at java.io.ObjectStreamClass.initNonProxy(ObjectStreamClass.java:699)
+	at java.io.ObjectInputStream.readNonProxyDesc(ObjectInputStream.java:2028)
+	at java.io.ObjectInputStream.readClassDesc(ObjectInputStream.java:1875)
+	at java.io.ObjectInputStream.readOrdinaryObject(ObjectInputStream.java:2209)
+	at java.io.ObjectInputStream.readObject0(ObjectInputStream.java:1692)
+	at java.io.ObjectInputStream.readObject(ObjectInputStream.java:508)
+	at java.io.ObjectInputStream.readObject(ObjectInputStream.java:466)
+	at pkgs.pkgExes.Exe001.m2(Exe001.java:58)
+	at pkgs.pkgExes.Exe001.main(Exe001.java:32)
 </>
 */
 
