d94bfdc512c932b7cd84d204acd73766bd0cf93e
Administrador <Administrador@MMW10LAB.mshome.net>
2025-04-26 08:25:59 -0400

    testing instanceof null instance

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,60 +1,5 @@
 package pkgs.pkgExes;
 
-import java.util.Objects;
-
-class Pessoa {
-	private int codigo;
-	private String nome;
-	public int getCodigo() {
-		return codigo;
-	}
-	public Pessoa() {
-		super();
-	}
-	public Pessoa(int codigo, String nome) {
-		super();
-		this.codigo = codigo;
-		this.nome = nome;
-	}
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	@Override
-	public String toString() {
-		System.out.println("Pessoa.toString()");
-		return "Pessoa [codigo=" + codigo + ", nome=" + nome + "]";
-	}
-	public boolean equals(Object obj) {
-		System.out.println("Pessoa.equals()");
-		System.out.println("[obj="+(obj)+"]");
-		boolean retorno = false;
-		if(obj == this) {
-			System.out.println("[ == ="+(true)+"]");
-			retorno = true;
-		}else {
-			System.out.println("[ == ="+(false)+"]");
-			if(obj instanceof Pessoa) {
-				System.out.println("[instanceof="+(true)+"]");
-				Pessoa pessoa = (Pessoa) obj;
-				System.out.println("[pessoa="+(pessoa)+"]");
-				if(pessoa.getCodigo() == this.getCodigo()) {
-					retorno = true;
-				}
-			}else {
-				System.out.println("[instanceof="+(false)+"]");
-			}
-		}
-		return retorno;
-	}
-
-}
-
 public class Exe001 {
 
 	public static void main(String[] args) {
@@ -65,8 +10,10 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		Pessoa pessoa1 = new Pessoa(1, "Marcio");
-		System.out.println("[="+(Objects.equals(pessoa1, null))+"]");
+		Object o = null;
+		System.out.println("[="+(o instanceof Object)+"]");
+		Object o2 = new Object();
+		System.out.println("[="+(o2 instanceof Object)+"]");
 	}
 
 }
@@ -74,11 +21,8 @@ public class Exe001 {
 /*
 <>
 Exe001.m1()
-Pessoa.equals()
-[obj=null]
-[ == =false]
-[instanceof=false]
 [=false]
+[=true]
 </>
 */
 
