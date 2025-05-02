c55426383b1aa1f01df663529a085d440d94883b
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-02 10:54:12 -0400

    Collections.sort

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,7 @@
 package pkgs.pkgExes;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Exe001 {
@@ -13,64 +14,15 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		System.out.println();
-		try {
-			System.out.println("testing Integer reference with equals null reference");
-			Integer obj1 = new Integer(1000);
-			Integer obj2 = null;
-			System.out.println("[="+(obj1.equals(obj2))+"]");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println();
-		try {
-			System.out.println("testing null reference with equals Integer reference");
-			Integer obj1 = null;
-			Integer obj2 = new Integer(1000);
-			System.out.println("[="+(obj1.equals(obj2))+"]");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println();
-		try {
-			System.out.println("testing Integer reference with == null reference");
-			Integer obj1 = new Integer(1000);
-			Integer obj2 = null;
-			System.out.println("[="+(obj1 == obj2)+"]");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println();
-		try {
-			System.out.println("testing int value with == null reference");
-			int i1 = 1000;
-			Integer obj1 = null;
-			System.out.println("[="+(i1 == obj1)+"]");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println();
-		try {
-			System.out.println("testing Generic List with primitive type");
-			//List<int> list = new ArrayList<>();//não é permitido tipo primitivo no Generic List
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println();
-		try {
-			System.out.println("testing Generic List with Integer type");
-			List<Integer> list = new ArrayList<>();
-			list.add(3);
-			list.add(4);
-			list.add(null);
-			list.add(1);
-			list.add(2);
-			list.add(5);
-			System.out.println("[="+(list.get(1))+"]");//get(int index);
-			System.out.println("[="+(list.indexOf(4))+"]");//indexOf(Object o);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		List<String> list = new ArrayList<String>();
+		list.add("dNomeComecandoComD");
+		list.add("bNomeComecandoComB");
+		list.add("eNomeComecandoComE");
+		list.add("aNomeComecandoComA");
+		list.add("cNomeComecandoComC");
+		System.out.println("[ñ ordenado="+(list)+"]");
+		Collections.sort(list);
+		System.out.println("[ordenado="+(list)+"]");
 	}
 
 }
@@ -78,28 +30,8 @@ public class Exe001 {
 /*
 <>
 Exe001.m1()
-
-testing Integer reference with equals null reference
-[=false]
-
-testing null reference with equals Integer reference
-java.lang.NullPointerException
-	at pkgs.pkgExes.Exe001.m1(Exe001.java:30)
-	at pkgs.pkgExes.Exe001.main(Exe001.java:10)
-
-testing Integer reference with == null reference
-[=false]
-
-testing int value with == null reference
-java.lang.NullPointerException
-	at pkgs.pkgExes.Exe001.m1(Exe001.java:48)
-
-	at pkgs.pkgExes.Exe001.main(Exe001.java:10)
-testing Generic List with primitive type
-
-testing Generic List with Integer type
-[=4]
-[=1]
+[ñ ordenado=[dNomeComecandoComD, bNomeComecandoComB, eNomeComecandoComE, aNomeComecandoComA, cNomeComecandoComC]]
+[ordenado=[aNomeComecandoComA, bNomeComecandoComB, cNomeComecandoComC, dNomeComecandoComD, eNomeComecandoComE]]
 </>
 */
 
