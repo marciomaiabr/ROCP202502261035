ccc7ea97928d57f1f4a04b5fdde1c4995b2b2c24
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-02 10:28:00 -0400

    testing Boxing , == , equals() , List

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,54 +1,7 @@
 package pkgs.pkgExes;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-
-class Pessoa {
-	private long codigo;
-	private String nome;
-	public Pessoa() { super(); }
-	public Pessoa(long codigo, String nome) { super(); this.codigo = codigo; this.nome = nome; }
-	public Pessoa(long codigo) { super(); this.codigo = codigo; }
-	public long getCodigo() {return codigo;}
-	public void setCodigo(long codigo) {this.codigo = codigo;}
-	public String getNome() {return nome;}
-	public void setNome(String nome) {this.nome = nome;}
-	@Override
-	public String toString() {
-		return "Pessoa [codigo=" + codigo + ", nome=" + nome + "]";
-	}
-	@Override
-	public int hashCode() {
-		System.out.println("Pessoa.hashCode()");
-		int hashCode = Objects.hashCode(codigo);
-		System.out.println("[hashCode="+(hashCode)+"]");
-		return hashCode;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		System.out.println("Pessoa.equals()[this="+(this)+"][obj="+(obj)+"]");
-		boolean isEquals = false;
-		if (obj == null) {
-			System.out.println("[(obj == null)="+(true)+"]");
-			isEquals = false;
-		}
-		if (this == obj) {
-			System.out.println("[(this == obj)="+(true)+"]");
-			isEquals = true;
-		}
-		if (obj instanceof Pessoa) {
-			System.out.println("[(obj instanceof Pessoa)="+(true)+"]");
-			if (((Pessoa) obj).getCodigo() == this.getCodigo()) {
-				System.out.println("[(((Pessoa) obj).getCodigo() == this.getCodigo())="+(true)+"]");
-				isEquals = true;
-			}
-		}
-		System.out.println("[isEquals="+(isEquals)+"]");
-		return true;//isEquals;
-	}
-}
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exe001 {
 
@@ -60,27 +13,64 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		Collection<Pessoa> collection = new HashSet<Pessoa>();
-		Pessoa pessoa1A = new Pessoa(1, "PessoaA");
-		Pessoa pessoa2B = new Pessoa(2, "PessoaB");
-		Pessoa pessoa1C = new Pessoa(1, "PessoaC");
-		Pessoa pessoa2D = new Pessoa(2, "PessoaD");
-		Pessoa pessoa3E = new Pessoa(3, "PessoaE");
-		System.out.println("\nadd(pessoa1A)");
-		collection.add(pessoa1A);
-		System.out.println("\nadd(pessoa2B)");
-		collection.add(pessoa2B);
-		System.out.println("\nadd(pessoa1C)");
-		collection.add(pessoa1C);
-		System.out.println("\nadd(pessoa2D)");
-		collection.add(pessoa2D);
-		System.out.println("\nadd(pessoa3E)");
-		collection.add(pessoa3E);
-		Set impl = ((Set)collection);
-		System.out.println("\ncontains");
-		System.out.println("[=" + (impl.contains(new Pessoa(2))) + "]");
-		System.out.println("\nforEach");
-		impl.forEach(System.out::println);
+		System.out.println();
+		try {
+			System.out.println("testing Integer reference with equals null reference");
+			Integer obj1 = new Integer(1000);
+			Integer obj2 = null;
+			System.out.println("[="+(obj1.equals(obj2))+"]");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+		try {
+			System.out.println("testing null reference with equals Integer reference");
+			Integer obj1 = null;
+			Integer obj2 = new Integer(1000);
+			System.out.println("[="+(obj1.equals(obj2))+"]");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+		try {
+			System.out.println("testing Integer reference with == null reference");
+			Integer obj1 = new Integer(1000);
+			Integer obj2 = null;
+			System.out.println("[="+(obj1 == obj2)+"]");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+		try {
+			System.out.println("testing int value with == null reference");
+			int i1 = 1000;
+			Integer obj1 = null;
+			System.out.println("[="+(i1 == obj1)+"]");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+		try {
+			System.out.println("testing Generic List with primitive type");
+			//List<int> list = new ArrayList<>();//não é permitido tipo primitivo no Generic List
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+		try {
+			System.out.println("testing Generic List with Integer type");
+			List<Integer> list = new ArrayList<>();
+			list.add(3);
+			list.add(4);
+			list.add(null);
+			list.add(1);
+			list.add(2);
+			list.add(5);
+			System.out.println("[="+(list.get(1))+"]");//get(int index);
+			System.out.println("[="+(list.indexOf(4))+"]");//indexOf(Object o);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
@@ -89,47 +79,27 @@ public class Exe001 {
 <>
 Exe001.m1()
 
-add(pessoa1A)
-Pessoa.hashCode()
-[hashCode=1]
+testing Integer reference with equals null reference
+[=false]
 
-add(pessoa2B)
-Pessoa.hashCode()
-[hashCode=2]
+testing null reference with equals Integer reference
+java.lang.NullPointerException
+	at pkgs.pkgExes.Exe001.m1(Exe001.java:30)
+	at pkgs.pkgExes.Exe001.main(Exe001.java:10)
 
-add(pessoa1C)
-Pessoa.hashCode()
-[hashCode=1]
-Pessoa.equals()[this=Pessoa [codigo=1, nome=PessoaC]][obj=Pessoa [codigo=1, nome=PessoaA]]
-[(obj instanceof Pessoa)=true]
-[(((Pessoa) obj).getCodigo() == this.getCodigo())=true]
-[isEquals=true]
+testing Integer reference with == null reference
+[=false]
 
-add(pessoa2D)
-Pessoa.hashCode()
-[hashCode=2]
-Pessoa.equals()[this=Pessoa [codigo=2, nome=PessoaD]][obj=Pessoa [codigo=2, nome=PessoaB]]
-[(obj instanceof Pessoa)=true]
-[(((Pessoa) obj).getCodigo() == this.getCodigo())=true]
-[isEquals=true]
+testing int value with == null reference
+java.lang.NullPointerException
+	at pkgs.pkgExes.Exe001.m1(Exe001.java:48)
 
-add(pessoa3E)
-Pessoa.hashCode()
-[hashCode=3]
+	at pkgs.pkgExes.Exe001.main(Exe001.java:10)
+testing Generic List with primitive type
 
-contains
-Pessoa.hashCode()
-[hashCode=2]
-Pessoa.equals()[this=Pessoa [codigo=2, nome=null]][obj=Pessoa [codigo=2, nome=PessoaB]]
-[(obj instanceof Pessoa)=true]
-[(((Pessoa) obj).getCodigo() == this.getCodigo())=true]
-[isEquals=true]
-[=true]
-
-forEach
-Pessoa [codigo=1, nome=PessoaA]
-Pessoa [codigo=2, nome=PessoaB]
-Pessoa [codigo=3, nome=PessoaE]
+testing Generic List with Integer type
+[=4]
+[=1]
 </>
 */
 
