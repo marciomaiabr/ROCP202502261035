cf0a05bda282a7c7e18382a7e06b9747ce79f7ac
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-04 10:03:06 -0400

    HashSet

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -3,8 +3,55 @@ package pkgs.pkgExes;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.TreeSet;
+
+class Pessoa {
+	private long codigo;
+	private String nome;
+	public Pessoa() { super(); }
+	public Pessoa(long codigo, String nome) { super(); this.codigo = codigo; this.nome = nome; }
+	public Pessoa(long codigo) { super(); this.codigo = codigo; }
+	public Pessoa(String nome) { super(); this.nome = nome; }
+	public long getCodigo() {return codigo;}
+	public void setCodigo(long codigo) {this.codigo = codigo;}
+	public String getNome() {return nome;}
+	public void setNome(String nome) {this.nome = nome;}
+	@Override
+	public String toString() {
+		return "[[class="+(this.getClass().getSimpleName())+"]"+"[codigo="+(codigo)+"]"+"[nome="+(nome)+"]]";
+	}
+	@Override
+	public int hashCode() {
+		System.out.println("Pessoa.hashCode()");
+		int hashCode = Objects.hashCode(codigo);
+		System.out.println("[hashCode="+(hashCode)+"]");
+		return hashCode;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("Pessoa.equals()[this="+(this)+"][obj="+(obj)+"]");
+		boolean isEquals = false;
+		if (obj == null) {
+			System.out.println("[(obj == null)="+(true)+"]");
+			isEquals = false;
+		} else if (this == obj) {
+			System.out.println("[(this == obj)="+(true)+"]");
+			isEquals = true;
+		} else if (obj instanceof Pessoa) {
+			System.out.println("[(obj instanceof Pessoa)="+(true)+"]");
+			if (((Pessoa) obj).getCodigo() == this.getCodigo()) {
+				System.out.println("[(((Pessoa) obj).getCodigo() == this.getCodigo())="+(true)+"]");
+				isEquals = true;
+			}
+		}
+		System.out.println("[isEquals="+(isEquals)+"]");
+		return isEquals;
+	}
+}
 
 public class Exe001 {
 
@@ -20,23 +67,31 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		List<Integer> list = new ArrayList<>();
-		list.add(10);
-		list.add(20);
-		list.add(30);
-		list.add(40);
-		list.add(50);
-		list.forEach(System.out::println);
+		Pessoa pessoa1A = new Pessoa(1, "PessoaA");
+		Pessoa pessoa2B = new Pessoa(2, "PessoaB");
+		Pessoa pessoa1C = new Pessoa(1, "PessoaC");
+		Pessoa pessoa4D = new Pessoa(4, "PessoaD");
+		Pessoa pessoa2E = new Pessoa(2, "PessoaE");
+		Pessoa pessoa6F = new Pessoa(6, "PessoaF");
 		System.out.println();
-		System.out.println("System.out.println(list.remove(2));");
-		System.out.println(list.remove(2));
+		Collection<Pessoa> collection = new HashSet<>();
+		System.out.println("\ncollection.add(pessoa1A)");
+		System.out.println(collection.add(pessoa1A));
+		System.out.println("\ncollection.add(pessoa2B)");
+		System.out.println(collection.add(pessoa2B));
+		System.out.println("\ncollection.add(pessoa1C)");
+		System.out.println(collection.add(pessoa1C));
+		System.out.println("\ncollection.add(pessoa4D)");
+		System.out.println(collection.add(pessoa4D));
+		System.out.println("\ncollection.add(pessoa2E)");
+		System.out.println(collection.add(pessoa2E));
+		System.out.println("\ncollection.add(pessoa6F)");
+		System.out.println(collection.add(pessoa6F));
 		System.out.println();
-		list.forEach(System.out::println);
+		collection.forEach(System.out::println);
 		System.out.println();
-		System.out.println("System.out.println(list.remove(new Integer(20)));");
-		System.out.println(list.remove(new Integer(20)));
-		System.out.println();
-		list.forEach(System.out::println);
+		System.out.println("collection.contains(new Pessoa(4, \"PessoaD\"))");
+		System.out.println(collection.contains(new Pessoa(4, "PessoaD")));
 	}
 
 }
