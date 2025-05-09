8952cb4c32363b52a14cd12e5a4b80bd63d7c1a6
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-09 08:39:11 -0400

    Map HashMap

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -3,7 +3,18 @@ package pkgs.pkgExes;
 import java.time.LocalDateTime;
 import java.util.*;
 
-import pkgs.pkg0.Pessoa;
+class Dog {
+	public Dog(String n) { name = n; }
+	public String name;
+	@Override
+	public boolean equals(Object o) { System.out.println("Dog.equals()"); return ( (o != null) && (o instanceof Dog) && (((Dog) o).name == name) ); }
+	@Override
+	public int hashCode() { System.out.println("Dog.hashCode()"); int hashCode = name.length(); System.out.println("[hashCode="+(hashCode)+"]"); return hashCode; }
+}
+
+class Cat {}
+
+enum Pets {DOG, CAT, HORSE}
 
 public class Exe001 {
 
@@ -19,18 +30,29 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		Collection<Pessoa> collection = new HashSet<>();
-		Pessoa.criaPessoas(collection);
+		Map<Object, Object> m = new HashMap<>();
+		System.out.println("m.put(\"k1\", new Dog(\"aiko\"))");
+		System.out.println(m.put("k1", new Dog("aiko")));
+		System.out.println("m.put(\"k2\", Pets.DOG)");
+		System.out.println(m.put("k2", Pets.DOG));
+		System.out.println("m.put(Pets.CAT, \"CAT key\")");
+		System.out.println(m.put(Pets.CAT, "CAT key"));
+		Dog d1 = new Dog("clover");
+		System.out.println("m.put(d1, \"Dog key\")");
+		System.out.println(m.put(d1, "Dog key"));
+		System.out.println("m.put(new Cat(), \"Cat key\")");
+		System.out.println(m.put(new Cat(), "Cat key"));
+
 		System.out.println();
-		System.out.println("collection.forEach(System.out::println)");
-		collection.forEach(System.out::println);
-		System.out.println();
-		System.out.println("\ncollection.contains(new Pessoa(9, \"Pessoa9C\"))");
-		System.out.println(collection.contains(new Pessoa(9, "Pessoa9C")));
-		System.out.println("\ncollection.contains(new Pessoa(5, \"Pessoa5E\"))");
-		System.out.println(collection.contains(new Pessoa(5, "Pessoa5E")));
-		System.out.println("\ncollection.contains(new Pessoa(1, \"Pessoa1G\"))");
-		System.out.println(collection.contains(new Pessoa(1, "Pessoa1G")));
+
+		System.out.println(m.get("k1"));
+		String k2 = "k2";
+		System.out.println(m.get(k2));
+		Pets p = Pets.CAT;
+		System.out.println(m.get(p));
+		System.out.println(m.get(d1));
+		System.out.println(m.get(new Cat()));
+		System.out.println(m.size());
 	}
 
 }
