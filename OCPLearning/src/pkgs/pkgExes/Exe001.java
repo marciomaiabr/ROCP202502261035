aad9e76b8120282f7a8e954b13e4f2032e7f1e79
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-10 09:45:51 -0400

    Map HashMap

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -23,42 +23,42 @@ public class Exe001 {
 
 		KeyPessoa keyPessoa1A = new KeyPessoa(1l);
 		KeyPessoa keyPessoa2B = new KeyPessoa(2l);
-		KeyPessoa keyPessoa3C = new KeyPessoa(3l);
-		KeyPessoa keyPessoa4D = new KeyPessoa(4l);
-		KeyPessoa keyPessoa5E = new KeyPessoa(5l);
+		KeyPessoa keyPessoa1C = new KeyPessoa(1l);
+		KeyPessoa keyPessoa2D = new KeyPessoa(2l);
 		KeyPessoa keyPessoa6F = new KeyPessoa(6l);
+		KeyPessoa keyPessoa5E = new KeyPessoa(5l);
 
 		Pessoa pessoa1A = new Pessoa(new Key(true), "Pessoa1A");
 		Pessoa pessoa2B = new Pessoa(new Key(true), "Pessoa2B");
-		Pessoa pessoa3C = new Pessoa(new Key(true), "Pessoa3C");
-		Pessoa pessoa4D = new Pessoa(new Key(true), "Pessoa4D");
-		Pessoa pessoa5E = new Pessoa(new Key(true), "Pessoa5E");
+		Pessoa pessoa1C = new Pessoa(new Key(true), "Pessoa1C");
+		Pessoa pessoa2D = new Pessoa(new Key(true), "Pessoa2D");
 		Pessoa pessoa6F = new Pessoa(new Key(true), "Pessoa6F");
+		Pessoa pessoa5E = new Pessoa(new Key(true), "Pessoa5E");
 
 		System.out.println("\n\nm.puts");
 		System.out.println("\nm.put(keyPessoa1A, pessoa1A)");
-		m.put(keyPessoa1A, pessoa1A);
+		System.out.println(m.put(keyPessoa1A, pessoa1A));
 		System.out.println("\nm.put(keyPessoa2B, pessoa2B)");
-		m.put(keyPessoa2B, pessoa2B);
-		System.out.println("\nm.put(keyPessoa3C, pessoa3C)");
-		m.put(keyPessoa3C, pessoa3C);
-		System.out.println("\nm.put(keyPessoa4D, pessoa4D)");
-		m.put(keyPessoa4D, pessoa4D);
-		System.out.println("\nm.put(keyPessoa5E, pessoa5E)");
-		m.put(keyPessoa5E, pessoa5E);
+		System.out.println(m.put(keyPessoa2B, pessoa2B));
+		System.out.println("\nm.put(keyPessoa1C, pessoa1C)");
+		System.out.println(m.put(keyPessoa1C, pessoa1C));
+		System.out.println("\nm.put(keyPessoa2D, pessoa2D)");
+		System.out.println(m.put(keyPessoa2D, pessoa2D));
 		System.out.println("\nm.put(keyPessoa6F, pessoa6F)");
-		m.put(keyPessoa6F, pessoa6F);
+		System.out.println(m.put(keyPessoa6F, pessoa6F));
+		System.out.println("\nm.put(keyPessoa5E, pessoa5E)");
+		System.out.println(m.put(keyPessoa5E, pessoa5E));
 
 		System.out.println();
 		System.out.println("\nm.forEach"+"\n");
 		m.forEach((k,v) -> System.out.println("[k="+(k)+"]"+"[v="+(v)+"]"));
 
 		System.out.println("\n\nm.gets");
-		System.out.println("\nm.get(new KeyPessoa(4l))");
-		System.out.println(m.get(new KeyPessoa(4l)));
+		System.out.println("\nm.get(keyPessoa5E)");
+		System.out.println(m.get(keyPessoa5E));
 	}
 }
 
 /*
-
+ele ordena pelo generic k(de key)
 */
