95e9b718efeb4a7fa2f645148856e1a109533765
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-11 10:27:53 -0400

    Queue PriorityQueue Pessoa Comparator

--- OCPLearning/src/pkgs/pkg0/Pessoa.java
+++ OCPLearning/src/pkgs/pkg0/Pessoa.java
@@ -2,7 +2,7 @@ package pkgs.pkg0;
 
 import java.util.*;
 
-public class Pessoa implements Comparable<Pessoa> {
+public class Pessoa {
 	private Id id;
 	private String nome;
 	private int idade;
@@ -44,13 +44,6 @@ public class Pessoa implements Comparable<Pessoa> {
 		}
 		System.out.println("[isEquals="+(isEquals)+"]");
 		return isEquals;
-	}
-	@Override
-	public int compareTo(Pessoa pessoa) {
-		System.out.println("Pessoa.compareTo()");
-		int intCompareTo = new Integer(this.getIdade()).compareTo(pessoa.getIdade());
-		System.out.println("[intCompareTo="+(intCompareTo)+"]");
-		return intCompareTo;
 	}
 }
 
