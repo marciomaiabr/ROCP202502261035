63b42bd9b0e115899ebe427cde7aa0a8b190e293
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-12 07:41:28 -0400

    ArrayDeque.offer with Pessoa class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -19,24 +19,40 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		List<Integer> nums = Arrays.asList(10,9,8,7,6,5);
-		ArrayDeque<Integer> a = new ArrayDeque<>();
-		ArrayDeque<Integer> b = new ArrayDeque<>();
-		ArrayDeque<Integer> c = new ArrayDeque<>();
-		ArrayDeque<Integer> d = new ArrayDeque<>();
-		ArrayDeque<Integer> e = new ArrayDeque<>();
-		for(Integer n : nums) {
-			a.offer(n);
-			b.offerFirst(n);
-			c.push(n);
-			d.add(n);
-			e.addFirst(n);
-		}
-		System.out.println("[a="+(a)+"]");
-		System.out.println("[b="+(b)+"]");
-		System.out.println("[c="+(c)+"]");
-		System.out.println("[d="+(d)+"]");
-		System.out.println("[e="+(e)+"]");
+
+		Pessoa pessoa1A15 = new Pessoa(new Id(1l), "Pessoa1A15", 15);
+		Pessoa pessoa2B18 = new Pessoa(new Id(2l), "Pessoa2B18", 18);
+		Pessoa pessoa3C18 = new Pessoa(new Id(3l), "Pessoa3C18", 18);
+		Pessoa pessoa4D18 = new Pessoa(new Id(4l), "Pessoa4D18", 18);
+		Pessoa pessoa5E47 = new Pessoa(new Id(5l), "Pessoa5E47", 47);
+		Pessoa pessoa6F16 = new Pessoa(new Id(6l), "Pessoa6F16", 16);
+		Pessoa pessoa7G14 = new Pessoa(new Id(7l), "Pessoa7G14", 14);
+
+		ArrayDeque<Pessoa> q = new ArrayDeque<>();
+
+		System.out.println("\n\nq.offer");
+
+		System.out.println("\nq.offer(pessoa1A15)");
+		System.out.println(q.offer(pessoa1A15));
+		System.out.println("\nq.offer(pessoa2B18)");
+		System.out.println(q.offer(pessoa2B18));
+		System.out.println("\nq.offer(pessoa4D18)");
+		System.out.println(q.offer(pessoa4D18));
+		System.out.println("\nq.offer(pessoa3C18)");
+		System.out.println(q.offer(pessoa3C18));
+		System.out.println("\nq.offer(pessoa5E47)");
+		System.out.println(q.offer(pessoa5E47));
+		System.out.println("\nq.offer(pessoa6F16)");
+		System.out.println(q.offer(pessoa6F16));
+		System.out.println("\nq.offer(pessoa7G14)");
+		System.out.println(q.offer(pessoa7G14));
+
+		System.out.println("\n\nq.peek");
+		System.out.println(q.peek());
+
+		System.out.println("\n\nq.polls");
+		while(q.size()>0)
+			System.out.println(q.poll());
 	}
 }
 
