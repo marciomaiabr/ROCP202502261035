c44defdafe2ec3390a3288f71075af0ca0abd12c
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-18 08:32:33 -0400

    testing generics and arrays

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,8 +1,7 @@
 package pkgs.pkgExes;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 abstract class Animal { public abstract void checkup(); }
 
@@ -10,7 +9,14 @@ class Dog extends Animal { public void checkup() { System.out.println("Dog check
 class Cat extends Animal { public void checkup() { System.out.println("Cat checkup"); }; }
 class Bird extends Animal { public void checkup() { System.out.println("Bird checkup"); }; }
 
-class AnimalDoctor { public void checkAnimals(ArrayList<Animal> animals) { for(Animal animal : animals) animal.checkup(); } }
+class AnimalDoctorWithArray {
+	public void checkAnimals(Animal [] animals) { System.out.println("AnimalDoctorWithArray.checkAnimals()"); for(Animal animal : animals) animal.checkup(); }
+	public void addCat(Animal [] animals) { System.out.println("AnimalDoctorWithArray.addCat()"); animals[0] = new Cat(); }
+}
+
+class AnimalDoctorWithGeneric {
+	public void checkAnimals(ArrayList<Animal> animals) { System.out.println("AnimalDoctorWithGeneric.checkAnimals()"); for(Animal animal : animals) animal.checkup(); }
+}
 
 public class Exe001 {
 
@@ -36,26 +42,36 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		List<Animal> dogs = new ArrayList<Animal>();
-		dogs.add(new Dog());
-		dogs.add(new Dog());
-		List<Animal> cats = new ArrayList<Animal>();
-		cats.add(new Cat());
-		cats.add(new Cat());
-		List<Animal> birds = new ArrayList<Animal>();
-		birds.add(new Bird());
 
-		AnimalDoctor animalDoctor = new AnimalDoctor();
-		animalDoctor.checkAnimals((ArrayList<Animal>) dogs);
-		animalDoctor.checkAnimals((ArrayList<Animal>) cats); 
-		animalDoctor.checkAnimals((ArrayList<Animal>) birds); 
+		{
+			System.out.println("testing with array...");
+			Dog [] dogs = new Dog[5];
+
+			AnimalDoctorWithArray animalDoctor = new AnimalDoctorWithArray();
+			animalDoctor.addCat(dogs);
+		}
+
+		/*{
+			System.out.println("testing with generic...");
+			ArrayList<Animal> dogs = new ArrayList<Animal>();
+			dogs.add(new Dog());
+			dogs.add(new Dog());
+			ArrayList<Animal> cats = new ArrayList<Animal>();
+			cats.add(new Cat());
+			cats.add(new Cat());
+			ArrayList<Animal> birds = new ArrayList<Animal>();
+			birds.add(new Bird());
+
+			AnimalDoctorWithGeneric animalDoctor = new AnimalDoctorWithGeneric();
+			animalDoctor.checkAnimals(dogs);
+			animalDoctor.checkAnimals(cats);
+			animalDoctor.checkAnimals(birds);
+		}*/
+
 	}
 
 }
 
 /*
-without
-Multiple markers at this line
-	- Type safety: The expression of type ArrayList needs unchecked conversion to conform to ArrayList<Animal>
-	- ArrayList is a raw type. References to generic type ArrayList<E> should be parameterized
+
 */
