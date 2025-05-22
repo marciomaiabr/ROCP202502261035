9e9ebb4966e6f8b06ac34356aeaac89f3de1b6d4
Administrador <Administrador@MMW10LAB.mshome.net>
2025-05-22 08:00:33 -0400

    my generic class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -3,98 +3,41 @@ package pkgs.pkgExes;
 import java.time.LocalDateTime;
 import java.util.*;
 
-class ListMM<E> {
-	public int size() {
-		return 0;
+class Rental {
+	private List rentalPool;
+	private int maxNum;
+	public Rental(int maxNum, List rentalPool) {
+		this.maxNum = maxNum;
+		this.rentalPool = rentalPool;
 	}
-
-	public boolean isEmpty() {
-		return false;
+	public Object getRental() {
+		return rentalPool.get(0);
 	}
-
-	public boolean contains(Object o) {
-		return false;
+	public void returnRental(Object o) {
+		rentalPool.add(o);
 	}
+}
 
-	public Iterator<E> iterator() {
-		return null;
+class Car {
+}
+
+class CarRental extends Rental {
+
+	public CarRental(int maxNum, List<Car> rentalPool) {
+		super(maxNum, rentalPool);
 	}
-
-	public Object[] toArray() {
-		return null;
+	public Car getRental() {
+		return (Car) super.getRental();
 	}
-
-	public <T> T[] toArray(T[] a) {
-		return null;
+	public void returnRental(Car car) {
+		super.returnRental(car);
 	}
-
-	public boolean add(E e) {
-		return false;
-	}
-
-	public void qqcMM1(E e) {
-	}
-
-	public boolean remove(Object o) {
-		return false;
-	}
-
-	public boolean containsAll(Collection<?> c) {
-		return false;
-	}
-
-	public boolean addAll(Collection<? extends E> c) {
-		return false;
-	}
-
-	public boolean addAll(int index, Collection<? extends E> c) {
-		return false;
-	}
-
-	public boolean removeAll(Collection<?> c) {
-		return false;
-	}
-
-	public boolean retainAll(Collection<?> c) {
-		return false;
-	}
-
-	public void clear() {
-	}
-
-	public E get(int index) {
-		return null;
-	}
-
-	public E set(int index, E element) {
-		return null;
-	}
-
-	public void add(int index, E element) {
-	}
-
-	public E remove(int index) {
-		return null;
-	}
-
-	public int indexOf(Object o) {
-		return 0;
-	}
-
-	public int lastIndexOf(Object o) {
-		return 0;
-	}
-
-	public ListIterator<E> listIterator() {
-		return null;
-	}
-
-	public ListIterator<E> listIterator(int index) {
-		return null;
-	}
-
-	public List<E> subList(int fromIndex, int toIndex) {
-		return null;
+	public void returnRental(Object o) {
+		if(o instanceof Car) {
+			super.returnRental(o);
+		} else {
+			throw new RuntimeException("MM RuntimeException Cannot add a non-Car");
+		}
 	}
 }
 
@@ -122,22 +65,12 @@ public class Exe001 {
 
 	public static void m1(String[] args) {
 		System.out.println("Exe001.m1()");
-		mqqc1(null);
-		mqqc1(null);
-	}
-
-	public static void mqqc1(ListMM<? extends Object> list) {
-		System.out.println("Exe001.mqqc1()");
-		list.qqcMM1(LocalDateTime.now());
-	}
-
-	public static void mqqc2(ListMM<? super Object> list) {
-		System.out.println("Exe001.mqqc2()");
-		list.qqcMM1(LocalDateTime.now());
+		CarRental carRental = null;
+		System.out.println("[="+(carRental)+"]");
 	}
 
 }
 
 /*
-eu acho q ele da erro de compilação ao adicionar, pq ele não sabe o que adicionar...
+
 */
