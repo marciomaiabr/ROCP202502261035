d95c1ab94dd1558a61e5612c30d53d3c75731a09
Administrador <Administrador@MMW10LAB.mshome.net>
2025-06-14 09:18:26 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -2,8 +2,6 @@ package pkgs.pkgExes;
 
 import java.time.LocalDateTime;
 
-class ClassA { public String mqqc1() { return "ClassA.mqqc1"; } }
-
 public class Exe001 {
 
 	static {
@@ -33,40 +31,7 @@ public class Exe001 {
 
 	public void im1(String[] args) {
 		System.out.println("Exe001.im1()");
-		qqc1(args);
-	}
-
-	String str = "s";
-
-	public void qqc1(String[] args) {
-		System.out.println("Exe001.qqc1()");
-
-		class MyLevel1Class1 extends ClassA {
-			private Integer integerX = 1;
-			@Override
-			public String mqqc1() { return "MyLevel1Class1.mqqc1"; }
-			@Override
-			public String toString() {
-				str = "z";
-				String retorno = "["
-						+"[this.getClass().getCanonicalName()="+(this.getClass().getCanonicalName())+"]"
-						+"[getClass().getCanonicalName()="+(getClass().getCanonicalName())+"]"
-						+"[this.integerX="+(this.integerX)+"]"
-						+"[integerX="+(integerX)+"]"
-						+"[this.mqqc1()="+(this.mqqc1())+"]"
-						+"[mqqc1()="+(mqqc1())+"]"
-						+"[super.mqqc1()="+(super.mqqc1())+"]"
-						+"[str="+(str)+"]"
-						+"\n"+"]";
-				retorno = retorno.replaceAll("\\[", "\n\t"+"\\[");
-				return retorno;
-			}
-
-		}
-
-		MyLevel1Class1 myLevel1Class1 = new MyLevel1Class1();
-		System.out.println("[myLevel1Class1]"+myLevel1Class1);
-
+		//System.out.println("[="+()+"]");
 	}
 
 }
