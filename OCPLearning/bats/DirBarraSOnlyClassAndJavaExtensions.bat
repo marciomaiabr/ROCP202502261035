85f96a056a96dd9c6eab70c0b4aab06c0d71c243
Administrador <Administrador@MMW10LAB.mshome.net>
2025-06-15 09:54:20 -0400

    print only folder and file

--- OCPLearning/bats/DirBarraSOnlyClassAndJavaExtensions.bat
+++ OCPLearning/bats/DirBarraSOnlyClassAndJavaExtensions.bat
@@ -1,4 +1,4 @@
 cls
 cd C:\Users\Administrador\git\ROCP202502261035\OCPLearning
-dir *.java /s  >logs\Sources.txt
-dir *.class /s  >logs\Classes.txt
+dir *.java /b/s  >logs\Sources.txt
+dir *.class /b/s  >logs\Classes.txt
