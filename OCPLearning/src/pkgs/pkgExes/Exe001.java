5b29c7dd1ede7e3d91962c7b37ccabbf95342546
Administrador <Administrador@MMW10LAB.mshome.net>
2025-07-02 08:26:00 -0400

    Streams without IllegalStateException

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -36,7 +36,7 @@ public class Exe001 {
 		Integer [] myNums = {1,2,3};
 		Stream<Integer> myStream = Arrays.stream(myNums);
 		System.out.println(myStream);
-		System.out.println(myStream.count());
+		//System.out.println(myStream.count());
 		System.out.println(myStream.filter(i -> i >1).count());
 	}
 
