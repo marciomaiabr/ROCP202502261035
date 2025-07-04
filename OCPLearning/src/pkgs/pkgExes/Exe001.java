afb9d32fefea3656a8fb067fefb46dec757edc08
Administrador <Administrador@MMW10LAB.mshome.net>
2025-07-04 08:28:16 -0400

    processa arquivo com Streams

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 class Pessoa {
 	private Long id;
@@ -61,22 +62,19 @@ class Pessoa {
 	public static List<Pessoa> loadPessoasListFromTxtFile(File file){
 		List<Pessoa> pessoas = new ArrayList<>();
 		System.out.println("Exe001.loadPessoas()");
-		Supplier<List<String>> rowSupplier = () -> {
-			try {
-				return Files.readAllLines(file.toPath());
-			} catch (Exception e) {
-				throw new RuntimeException("Falha ao ler fonte de dados", e);
-			}
-		};
 		Consumer<String> rowProcess = row -> {
 			try {
 				String [] rec = row.split(";");
 				pessoas.add(new Pessoa(Long.parseLong(rec[0]), rec[1], rec[2].charAt(0), Integer.parseInt(rec[3])));
 			} catch (Exception e) {
-				throw new RuntimeException("Falha ao processar registros", e);
+				throw new RuntimeException("Falha ao processar registro", e);
 			}
 		};
-		rowSupplier.get().forEach(rowProcess);
+		try (Stream<String> stream = Files.lines(file.toPath())) {
+			stream.forEach(rowProcess);
+		} catch (Exception e) {
+			throw new RuntimeException("Falha ao processar registros", e);
+		}
 		return pessoas;
 	}
 
