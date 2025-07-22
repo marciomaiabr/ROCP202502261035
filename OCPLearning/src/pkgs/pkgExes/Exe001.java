3a3fe5ab68148f01d1ee9fb74bd5d1ab4b46f5bc
Administrador <Administrador@MMW10LAB.mshome.net>
2025-07-22 07:53:00 -0400

    Streams collect Collectors.counting

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -86,8 +86,9 @@ public class Exe001 {
 		System.out.println(Stream.of(arr).collect(Collectors.groupingBy(Person::getState, Collectors.groupingBy(Person::getCity, Collectors.counting()))));
 		System.out.println(Stream.of(arr).collect(Collectors.groupingBy(Person::getState, Collectors.groupingBy(Person::getCity, Collectors.toList()))));
 		System.out.println(Stream.of(arr).collect(Collectors.groupingBy(Person::getState, Collectors.groupingBy(Person::getCity, Collectors.mapping(Person::getName, Collectors.toList())))));
-		System.out.println(Stream.of(arr).collect(Collectors.partitioningBy(p->p.getCity().equals("CBA"))));*/
-		System.out.println(Stream.of(arr).filter(v -> v.getName().startsWith("B")).collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge))));
+		System.out.println(Stream.of(arr).collect(Collectors.partitioningBy(p->p.getCity().equals("CBA"))));
+		System.out.println(Stream.of(arr).filter(v -> v.getName().startsWith("B")).collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge))));*/
+		System.out.println(Stream.of(arr).collect(Collectors.counting()));
 	}
 
 }
