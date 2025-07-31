f3858e43776d9568e118bb9be6726cd7fc7fc48a
Administrador <Administrador@MMW10LAB.mshome.net>
2025-07-31 09:15:30 -0400

    Threads testing example

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -2,6 +2,60 @@ package pkgs.pkgExes;
 
 import java.time.LocalDateTime;
 
+class Account {
+    private int balance = 50;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+}
+
+class AccountDanger implements Runnable {
+    private Account acct = new Account();
+
+    public static void main(String[] args) {
+        AccountDanger r = new AccountDanger();
+        Thread one = new Thread(r);
+        Thread two = new Thread(r);
+        one.setName("Fred");
+        two.setName("Lucy");
+        one.start();
+        two.start();
+    }
+
+    public void run() {
+        for (int x = 0; x < 5; x++) {
+            makeWithdrawal(10);
+            if (acct.getBalance() < 0) {
+                System.out.println("account is overdrawn!");
+            }
+        }
+    }
+
+    private void makeWithdrawal(int amt) {
+        if (acct.getBalance() >= amt) {
+            System.out.println(Thread.currentThread().getName()
+                + " is going to withdraw");
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) { }
+
+            acct.withdraw(amt);
+            System.out.println(Thread.currentThread().getName()
+                + " completes the withdrawal");
+        } else {
+            System.out.println("Not enough in account for "
+                + Thread.currentThread().getName()
+                + " to withdraw " + acct.getBalance());
+        }
+    }
+}
+
 public class Exe001 {
 
 	static {
@@ -31,27 +85,7 @@ public class Exe001 {
 
 	public void im1(String[] args) {
 		System.out.println("Exe001.im1()");
-
-		System.out.println("[aki][1]");
-
-		Thread threadAA = new Thread(()->{
-			System.out.println("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][inicio]");
-			try {
-				Thread.sleep(3*1000);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			System.out.println("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][fim]");
-		}, "threadAA");
-		threadAA.start();
-		try {
-			threadAA.join();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("[aki][2]");
-
+		AccountDanger.main(args);
 	}
 
 }
