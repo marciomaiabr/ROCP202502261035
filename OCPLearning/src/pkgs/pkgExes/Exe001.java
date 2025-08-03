2a63c0b1e262ad5ad3143600af5ac04926c2930f
Administrador <Administrador@MMW10LAB.mshome.net>
2025-08-03 10:30:35 -0400

    Threads AtomicInteger

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,64 +1,7 @@
 package pkgs.pkgExes;
 
 import java.time.LocalDateTime;
-
-class Calculator implements Runnable {
-    int total;
-
-	@Override
-	public void run() {
-		System.out.println("[Calculator]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][1]");
-        synchronized(this) {
-    		System.out.println("[Calculator]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][2]");
-            for(int i = 0; i < 100; i++) {
-                total += i;
-            }
-    		System.out.println("[Calculator]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][3]");
-            notifyAll();
-    		System.out.println("[Calculator]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][4]");
-        }
-		System.out.println("[Calculator]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][5]");
-    }
-}
-
-class Reader extends Thread {
-	Calculator calculator2;
-
-    public Reader(Calculator pCalculator, String name) {
-    	this.calculator2 = pCalculator;
-    	setName(name);
-    }
-
-	@Override
-	public void run() {
-		System.out.println("[Reader]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][1]");
-        synchronized(calculator2) {
-    		System.out.println("[Reader]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][2]");
-            try {
-        		System.out.println("[Reader]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][3]");
-        		calculator2.wait();
-        		System.out.println("[Reader]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][4]");
-            } catch (Exception e) {
-            	e.printStackTrace();
-            }
-    		System.out.println("[Reader]"+"["+(Thread.currentThread().getName())+"]"+"[run()][aki][5]");
-        }
-    }
-
-    public static void main(String [] args) {
-		System.out.println("[Reader][main(String [] args)][aki][1]");
-        Calculator calculator1 = new Calculator();
-		System.out.println("[Reader][main(String [] args)][aki][2]");
-        new Reader(calculator1, "R1").start();
-		System.out.println("[Reader][main(String [] args)][aki][3]");
-        new Reader(calculator1, "R2").start();
-		System.out.println("[Reader][main(String [] args)][aki][4]");
-        new Reader(calculator1, "R3").start();
-		System.out.println("[Reader][main(String [] args)][aki][5]");
-        new Thread(calculator1, "T3").start();
-		System.out.println("[Reader][main(String [] args)][aki][6]");
-    }
-}
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Exe001 {
 
@@ -89,7 +32,8 @@ public class Exe001 {
 
 	public void im1(String[] args) {
 		System.out.println("Exe001.im1()");
-		Reader.main(args);
+		AtomicInteger ai = new AtomicInteger();
+		System.out.println(ai);
 	}
 
 }
