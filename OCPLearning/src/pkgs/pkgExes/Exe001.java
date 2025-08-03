c82569a3a8116664af64ca46476d82ca140b3011
Administrador <Administrador@MMW10LAB.mshome.net>
2025-08-03 08:42:40 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -2,54 +2,6 @@ package pkgs.pkgExes;
 
 import java.time.LocalDateTime;
 
-class InstancePrinter{
-	private String instanceName;
-	InstancePrinter(String instanceName){
-		this.instanceName = instanceName;
-	}
-	public synchronized void print() {
-		System.out.println("[InstancePrinter][instanceName="+(instanceName)+"][start]");
-		try {
-			Thread.sleep(5*1000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("[InstancePrinter][instanceName="+(instanceName)+"][end]");
-	}
-}
-
-class StaticPrinter{
-	public synchronized static void print() {
-		System.out.println("[StaticPrinter][start]");
-		try {
-			Thread.sleep(5*1000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("[StaticPrinter][end]");
-	}
-}
-
-class ThreadMM extends Thread {
-	private InstancePrinter instancePrinter;
-	private String name;
-	public ThreadMM(String name, InstancePrinter instancePrinter) {
-		super(name);
-		this.name = name;
-		this.instancePrinter = instancePrinter;
-	}
-	@Override
-	public void run() {
-		System.out.println("["+(name)+"][run][start]");
-		if(instancePrinter == null){
-			StaticPrinter.print();
-		}else{
-			instancePrinter.print();
-		}
-		System.out.println("["+(name)+"][run][end]");
-	}
-}
-
 public class Exe001 {
 
 	static {
@@ -79,13 +31,6 @@ public class Exe001 {
 
 	public void im1(String[] args) {
 		System.out.println("Exe001.im1()");
-		/*testar com instancias diferente de impressoras e mesma Thread
-		testar com instancias diferente de impressoras e instancias diferente de Thread
-		testar com mesma instancias de impressoras e diferente Thread*/
-		InstancePrinter ip = new InstancePrinter("ip");
-		Thread t = new Thread(()->{
-			ip.print();
-		}, "t");
 	}
 
 }
