62edad2c8d5401f52ac07e9e7def563d53576a70
Administrador <Administrador@MMW10LAB.mshome.net>
2025-08-11 11:07:34 -0400

    Threads Deadlock example in synchronized method

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -2,6 +2,37 @@ package pkgs.pkgExes;
 
 import java.time.LocalDateTime;
 
+class RecursoA {
+	public synchronized void mA1(RecursoB b) {
+		try {
+			Thread.sleep(1*1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		b.mB1();
+	}
+	public synchronized void mA2() {}
+}
+
+class RecursoB {
+	public synchronized void mB1() {
+		try {
+			Thread.sleep(1*1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public synchronized void mB2(RecursoA a) {
+		try {
+			Thread.sleep(1*1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		a.mA2();
+	}
+}
+
 public class Exe001 {
 
 	static {
@@ -12,7 +43,7 @@ public class Exe001 {
 
 	public Exe001(String label) { this.label = label; }
 	@Override
-	public String toString() { return ("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"]"+"[label="+(label)+"]"); }
+	public String toString() { return ("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"]"+"[label="+(label)+"]"+"[super.toString()="+(super.toString())+"]"); }
 
 	public static void main(String[] args) {
 
@@ -38,85 +69,20 @@ public class Exe001 {
 
 	public void im1(String[] args) {
 		System.out.println("Exe001.im1()");
+        RecursoA recursoA = new RecursoA();
+        RecursoB recursoB = new RecursoB();
 
-		Object object1 = new Object();
-		Object object2 = new Object();
-		Object object3 = new Object();
-		Object object4 = new Object();
+        new Thread(() -> {
+        	recursoA.mA1(recursoB);
+        }).start();
+        new Thread(() -> {
+        	recursoB.mB2(recursoA);
+        }).start();
 
-		new Thread(()->{
-			log(1);
-			sleep(1000);
-	        synchronized (object1) {
-	        	log(2);
-				sleep(1000);
-	            synchronized (object2) {
-	            	log(3);
-	    			sleep(1000);
-		            synchronized (object3) {
-		            	log(4);
-		    			sleep(1000);
-			            synchronized (object4) {
-			            	log(5);
-			    			sleep(1000);
-			            }
-		            }
-	            }
-	        }
-	    }).start();
-
-		new Thread(()->{
-			log(1);
-			sleep(1000);
-	        synchronized (object4) {
-	        	log(2);
-				sleep(1000);
-	            synchronized (object3) {
-	            	log(3);
-	    			sleep(1000);
-		            synchronized (object2) {
-		            	log(4);
-		    			sleep(1000);
-			            synchronized (object1) {
-			            	log(5);
-			    			sleep(1000);
-			            }
-		            }
-	            }
-	        }
-	    }).start();
-
-	}
-
-	private void log(int point) {
-		System.out.println("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"]"+"[point="+(point)+"]");
-	}
-	public static void sleep(long millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (Exception e) {
-			throw new RuntimeException("falha no sleep", e);
-		}
 	}
 
 }
 
 /*
-"Thread-1" #15 prio=5 os_prio=0 tid=0x0000026ebf6be800 nid=0x1948 waiting for monitor entry [0x00000087672ff000]
-   java.lang.Thread.State: BLOCKED (on object monitor)
-        at pkgs.pkgExes.Exe001.lambda$1(Exe001.java:78)
-        - waiting to lock <0x00000000e08d32c8> (a java.lang.Object)
-        - locked <0x00000000e08d32d8> (a java.lang.Object)
-        - locked <0x00000000e08d32e8> (a java.lang.Object)
-        at pkgs.pkgExes.Exe001$$Lambda$5/1864350231.run(Unknown Source)
-        at java.lang.Thread.run(Thread.java:750)
 
-"Thread-0" #14 prio=5 os_prio=0 tid=0x0000026ebf6be000 nid=0x1868 waiting for monitor entry [0x00000087671ff000]
-   java.lang.Thread.State: BLOCKED (on object monitor)
-        at pkgs.pkgExes.Exe001.lambda$0(Exe001.java:57)
-        - waiting to lock <0x00000000e08d32d8> (a java.lang.Object)
-        - locked <0x00000000e08d32c8> (a java.lang.Object)
-        - locked <0x00000000e08d32b8> (a java.lang.Object)
-        at pkgs.pkgExes.Exe001$$Lambda$4/1050349584.run(Unknown Source)
-        at java.lang.Thread.run(Thread.java:750)
 */
