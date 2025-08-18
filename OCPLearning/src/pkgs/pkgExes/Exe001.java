b5900d53ad0346ca1e417b31943542e70c39f243
Administrador <Administrador@MMW10LAB.mshome.net>
2025-08-18 14:23:47 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,26 +1,28 @@
 package pkgs.pkgExes;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.CyclicBarrier;
 
 public class Exe001 {
+
+	private String stringExe001Name;
 
 	static {
 		System.out.println(LocalDateTime.now());
 	}
 
-	private String label;
+	public Exe001() {}
 
-	public Exe001(String label) { this.label = label; }
+	public Exe001(String stringExe001Name) { this.stringExe001Name = stringExe001Name; }
+
 	@Override
-	public String toString() { return ("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"]"+"[label="+(label)+"]"+"[super.toString()="+(super.toString())+"]"); }
+	public String toString() { return ("[Exe001][toString()][stringExe001Name="+(stringExe001Name)+"][Thread.currentThread().getName()="+(Thread.currentThread().getName())+"]"+"[super.toString()="+(super.toString())+"]"); }
 
 	public static void main(String[] args) {
 
 		try {
 			System.out.println("<>");
 			sm1(args);
-			Exe001 exe001 = new Exe001("exe001");
+			Exe001 exe001 = new Exe001();
 			exe001.im1(args);
 			System.out.println("</>");
 		} catch (Exception e) {
@@ -37,42 +39,8 @@ public class Exe001 {
 		System.out.println("Exe001.sm1()");
 	}
 
-
-
 	public void im1(String[] args) {
 		System.out.println("Exe001.im1()");
-
-		System.out.println("[Exe001][im1][Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][1]");
-
-		CyclicBarrier barrier = new CyclicBarrier(3, () -> {
-            System.out.println("[CyclicBarrier][Thread.currentThread().getName()="+(Thread.currentThread().getName())+"]");
-        });
-
-		System.out.println("[Exe001][im1][Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][2]");
-
-		Runnable runnable = ()->{
-			System.out.println("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][1]");
-			try {
-				Thread.sleep(1*1000);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			System.out.println("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][2]");
-			try {
-				barrier.await();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			System.out.println("[Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][3]");
-		};
-
-		System.out.println("[Exe001][im1][Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][3]");
-
-		new Thread(runnable,"t1").start();
-		new Thread(runnable,"t2").start();
-		new Thread(runnable,"t3").start();
-
-		System.out.println("[Exe001][im1][Thread.currentThread().getName()="+(Thread.currentThread().getName())+"][4]");
 	}
 
 }
