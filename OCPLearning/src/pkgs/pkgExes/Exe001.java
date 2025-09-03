071486d395656f3171e1c347d04c54320311ca2f
Administrador <Administrador@MMW10LAB.mshome.net>
2025-09-03 07:58:18 -0400

    JDBC before 4 with Class.forName

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -13,6 +13,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 		try {
+			Class.forName("com.mysql.jdbc.Driver");
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ocp202509020748", "root", "senha123");
 			ResultSet resultSet = connection.prepareStatement("select * from pessoa").executeQuery();
 
