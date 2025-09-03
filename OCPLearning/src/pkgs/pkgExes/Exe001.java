94996b21428de64ad6347394e5ef00e5bc77330d
Administrador <Administrador@MMW10LAB.mshome.net>
2025-09-03 07:56:17 -0400

    JDBC before 4 SQLException: No suitable driver found

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,5 +1,8 @@
 package pkgs.pkgExes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.time.LocalDateTime;
 
 public class Exe001 {
@@ -9,7 +12,16 @@ public class Exe001 {
 	}
 
 	public static void main(String[] args) {
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ocp202509020748", "root", "senha123");
+			ResultSet resultSet = connection.prepareStatement("select * from pessoa").executeQuery();
 
+			if(resultSet.next())
+				System.out.println("[connection="+(resultSet.getString("nome"))+"]"+"");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
