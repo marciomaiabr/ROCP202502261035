ca71e93e71a5d41b45c7b5904c66548fc5292d1c
Administrador <Administrador@MMW10LAB.mshome.net>
2025-09-10 05:07:28 -0400

    JDBC ResultSetMetaData

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -3,6 +3,7 @@ package pkgs.pkgExes;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.time.LocalDateTime;
 
@@ -15,54 +16,14 @@ public class Exe001 {
 	public static void main(String[] args) {
 		try {
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ocp202509020748", "root", "senha123");
-			Statement statement = null;
-			ResultSet resultSet = null;
-			boolean r = false;
-			int intRowSAffected = -7;
+			System.out.println("[connection="+(connection)+"]"+"");
 
-			statement = connection.createStatement();
-			r = statement.execute("update pessoa set nome = CONCAT(\"Marcio2\", \" \", \"B Maia\") where id in (1)");
-			System.out.println("[r="+(r)+"]"+"");
-
-			resultSet = statement.getResultSet();
+			Statement statement = connection.createStatement();
+			System.out.println("[statement="+(statement)+"]"+"");
+			ResultSet resultSet = statement.executeQuery("select * from pessoa");
 			System.out.println("[resultSet="+(resultSet)+"]"+"");
-			if( (resultSet != null) && resultSet.next())
-				System.out.println("[resultSet.getString(\"nome\")="+(resultSet.getString("nome"))+"]"+"");
-
-			intRowSAffected = statement.getUpdateCount();
-			System.out.println("[intRowSAffected="+(intRowSAffected)+"]"+"");
-			System.out.println();
-
-
-
-			statement = connection.createStatement();
-			r = statement.execute("select * from pessoa");
-			System.out.println("[r="+(r)+"]"+"");
-
-			resultSet = statement.getResultSet();
-			System.out.println("[resultSet="+(resultSet)+"]"+"");
-			if( (resultSet != null) && resultSet.next())
-				System.out.println("[resultSet.getString(\"nome\")="+(resultSet.getString("nome"))+"]"+"");
-
-			intRowSAffected = statement.getUpdateCount();
-			System.out.println("[intRowSAffected="+(intRowSAffected)+"]"+"");
-			System.out.println();
-
-
-
-			statement = connection.createStatement();
-			r = statement.execute("CREATE TABLE pessoa3 (id INT AUTO_INCREMENT PRIMARY KEY,nome VARCHAR(100) NOT NULL);");
-			System.out.println("[r="+(r)+"]"+"");
-
-			resultSet = statement.getResultSet();
-			System.out.println("[resultSet="+(resultSet)+"]"+"");
-			if( (resultSet != null) && resultSet.next())
-				System.out.println("[resultSet.getString(\"nome\")="+(resultSet.getString("nome"))+"]"+"");
-
-			intRowSAffected = statement.getUpdateCount();
-			System.out.println("[intRowSAffected="+(intRowSAffected)+"]"+"");
-			System.out.println();
-
+			ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+			System.out.println("[resultSetMetaData="+(resultSetMetaData)+"]"+"");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
