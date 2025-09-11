09368740e92a4fc100060d3cfcf3859a18be6b8e
Administrador <Administrador@MMW10LAB.mshome.net>
2025-09-11 06:29:47 -0400

    JDBC SQLException trying to move cursor

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 
 public class Exe001 {
@@ -17,34 +18,19 @@ public class Exe001 {
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ocp202509020748", "root", "senha123");
 			System.out.println("[connection="+(connection)+"]"+"");
 
-			DatabaseMetaData dbmd = connection.getMetaData();
+			Statement statement = connection.createStatement();
+			System.out.println("[statement="+(statement)+"]"+"");
 
-			if (dbmd.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY)) {
-			    System.out.print("Supports TYPE_FORWARD_ONLY");
-			    if (dbmd.supportsResultSetConcurrency(
-			            ResultSet.TYPE_FORWARD_ONLY,
-			            ResultSet.CONCUR_UPDATABLE)) {
-			    	System.out.println(" and supports CONCUR_UPDATABLE");
-			    }
-			}
+			ResultSet resultSet = statement.executeQuery("select * from pessoa");
+			System.out.println("[resultSet="+(resultSet)+"]"+"");
 
-			if (dbmd.supportsResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE)) {
-				System.out.print("Supports TYPE_SCROLL_INSENSITIVE");
-			    if (dbmd.supportsResultSetConcurrency(
-			            ResultSet.TYPE_SCROLL_INSENSITIVE,
-			            ResultSet.CONCUR_UPDATABLE)) {
-			    	System.out.println(" and supports CONCUR_UPDATABLE");
-			    }
-			}
-
-			if (dbmd.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE)) {
-				System.out.print("Supports TYPE_SCROLL_SENSITIVE");
-			    if (dbmd.supportsResultSetConcurrency(
-			            ResultSet.TYPE_SCROLL_SENSITIVE,
-			            ResultSet.CONCUR_UPDATABLE)) {
-			    	System.out.println("Supports CONCUR_UPDATABLE");
-			    }
-			}
+			System.out.println("[resultSet.getRow()="+(resultSet.getRow())+"]"+"");
+			System.out.println("[resultSet.next()="+(resultSet.next())+"]"+"");
+			System.out.println("[resultSet.last()="+(resultSet.last())+"]"+"");
+			System.out.println("[resultSet.getRow()="+(resultSet.getRow())+"]"+"");
+			System.out.println("[resultSet.next()="+(resultSet.next())+"]"+"");
+			resultSet.beforeFirst();
+			System.out.println("[resultSet.next()="+(resultSet.next())+"]"+"");
 
 		} catch (Exception e) {
 			e.printStackTrace();
