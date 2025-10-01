feeb989cde4b9016df9108d624db4c9a2e0c2cc0
Administrador <Administrador@MMW10LAB.mshome.net>
2025-10-01 07:34:02 -0400

    remembering instance block

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,42 +1,33 @@
 package pkgs.pkgExes;
 
 class ClassA {
-	static { System.out.println("static ClassA 1 "); }
-	public final static String A = "a";
-	public static void msClassA() { System.out.println("msClassA()"); }
+	static { System.out.println("static block ClassA 1 "); }
+	{ System.out.println("instance block ClassA 1 "); }
 	public ClassA() { System.out.println("ClassA()"); }
-	static { System.out.println("static ClassA 2 "); }
+	static { System.out.println("static block ClassA 2 "); }
+	{ System.out.println("instance block ClassA 2 "); }
 }
 
 class ClassB extends ClassA {
-	static { System.out.println("static ClassB 1 "); }
-	public final static String B = "b";
-	public static void msClassB() { System.out.println("msClassB()"); }
+	static { System.out.println("static block ClassB 1 "); }
+	{ System.out.println("instance block ClassB 1 "); }
 	public ClassB() { System.out.println("ClassB()"); }
-	static { System.out.println("static ClassB 2 "); }
+	static { System.out.println("static block ClassB 2 "); }
+	{ System.out.println("instance block ClassB 2 "); }
 }
 
 class ClassC extends ClassB {
-	static { System.out.println("static ClassC 1 "); }
-	public final static String C = "c";
-	public static void msClassC() { System.out.println("msClassC()"); }
+	static { System.out.println("static block ClassC 1 "); }
+	{ System.out.println("instance block ClassC 1 "); }
 	public ClassC() { System.out.println("ClassC()"); }
-	static { System.out.println("static ClassC 2 "); }
+	static { System.out.println("static block ClassC 2 "); }
+	{ System.out.println("instance block ClassC 2 "); }
 }
 
 public class Exe001 {
 
 	public static void main(String[] args) {
-		System.out.println(ClassC.C);
-		System.out.println();
-		System.out.println(ClassC.A);
-		System.out.println();
-
-		ClassC.msClassC();
+		new ClassC();
 	}
 
 }
-
-/*
-
-*/
