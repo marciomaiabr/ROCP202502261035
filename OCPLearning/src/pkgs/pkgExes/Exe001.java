a61625c33486dc5a4a851b369406c2cae8e4c4c0
Administrador <Administrador@MMW10LAB.mshome.net>
2025-10-01 07:23:22 -0400

    remembering block and method static

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,32 +1,39 @@
 package pkgs.pkgExes;
 
-import java.time.LocalDateTime;
+class ClassA {
+	static { System.out.println("static ClassA 1 "); }
+	public final static String A = "a";
+	public static void msClassA() { System.out.println("msClassA()"); }
+	public ClassA() { System.out.println("ClassA()"); }
+	static { System.out.println("static ClassA 2 "); }
+}
+
+class ClassB extends ClassA {
+	static { System.out.println("static ClassB 1 "); }
+	public final static String B = "b";
+	public static void msClassB() { System.out.println("msClassB()"); }
+	public ClassB() { System.out.println("ClassB()"); }
+	static { System.out.println("static ClassB 2 "); }
+}
+
+class ClassC extends ClassB {
+	static { System.out.println("static ClassC 1 "); }
+	public final static String C = "c";
+	public static void msClassC() { System.out.println("msClassC()"); }
+	public ClassC() { System.out.println("ClassC()"); }
+	static { System.out.println("static ClassC 2 "); }
+}
 
 public class Exe001 {
 
-	static {
-		System.out.println(LocalDateTime.now());
-	}
-
 	public static void main(String[] args) {
-		sm1(args);
-		new Exe001().im1(args);
+		System.out.println(ClassC.C);
+		System.out.println();
+		System.out.println(ClassC.A);
+		System.out.println();
+
+		ClassC.msClassC();
 	}
-
-	public static void sm1(String[] args) {
-		System.out.println("Exe001.sm1()");
-		//System.out.println("[="+()+"]"+"");
-	}
-
-	void m1(int i) {System.out.println("Exe001.m1(int i)");}
-	void m1(Integer i) {System.out.println("Exe001.m1(Integer i)");}
-	void m2(Integer i) {System.out.println("Exe001.m2(Integer i)");}
-
-	public void im1(String[] args) {
-		System.out.println("Exe001.im1()");
-		m1(5);
-		m2(5);
- 	}
 
 }
 
