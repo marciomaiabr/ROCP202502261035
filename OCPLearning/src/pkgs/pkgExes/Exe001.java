a25b1e2bf61f329b46ff42f74c9b6b7718c81891
Administrador <Administrador@MMW10LAB.mshome.net>
2025-10-06 08:07:49 -0400

    remembering try resource

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -2,18 +2,28 @@ package pkgs.pkgExes;
 
 class ClassA implements AutoCloseable {
 
-	@Override public void close() throws Exception { System.out.println("ClassA.close()..."); }
+	@Override public void close() throws Exception { System.out.println("ClassA.close()..."); throw new RuntimeException("RuntimeException close()..."); }
 
 }
 
 public class Exe001 {
 
 	public static void main(String[] args) {
+		try {
+			new Exe001().mtd();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void mtd() throws Exception {
 
 		try (ClassA classA = new ClassA();) {
 			System.out.println("try...");
+			throw new RuntimeException("RuntimeException try...");
 		} catch (Exception e) {
 			System.out.println("catch...");
+			throw new RuntimeException("RuntimeException catch...", e);
 		} finally {
 			System.out.println("finally...");
 		}
