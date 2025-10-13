3532443b7b0acda5aba53dd3754474a14fb72190
Administrador <Administrador@MMW10LAB.mshome.net>
2025-10-13 07:41:11 -0400

    remembering DateTime

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,9 +1,14 @@
 package pkgs.pkgExes;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Exe001 {
 
 	public static void main(String[] args) {
-
+		System.out.println(LocalDateTime.of(2017,10,27,14,22,54,0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh-mm-ss")));
+		System.out.println(LocalDateTime.of(2017,10,27,14,22,54,0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh-mm-ss a")));
+		//System.out.println(LocalDateTime.of(2017,10,27,14,22,54,0).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh-mm-ss z")));//DateTimeException: Unable to extract value: class java.time.LocalDateTime
 	}
 
 }
