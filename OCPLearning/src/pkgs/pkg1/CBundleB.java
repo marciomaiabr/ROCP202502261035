5bf0ced7e19466911ebb231266851a84c298d1db
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-04 07:47:14 -0400

    remembering ConcurrentMap

--- OCPLearning/src/pkgs/pkg1/CBundleB.java
+++
@@ -1,7 +0,0 @@
-package pkgs.pkg1;
-
-public class CBundleB extends java.util.ListResourceBundle {
-    protected Object[][] getContents() {
-        return new Object[][] { { 12, 34 } , { 56, 78 } };
-    }
-}
--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,52 +1,32 @@
 package pkgs.pkgExes;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 
-		ResourceBundle frbA = ResourceBundle.getBundle("FBundleA", Locale.getDefault());
-		frbA.keySet().forEach(System.out::println);
-		System.out.println();
-		/*System.out.println(frbA);
-		//System.out.println(frbA.getObject("12"));//MissingResourceException: Can't find resource for bundle java.util.PropertyResourceBundle, key 12
-		System.out.println(frbA.getObject("\"12\""));
-		System.out.println(frbA.getString("\"56\""));*/
-
-		ResourceBundle frbB = ResourceBundle.getBundle("FBundleB", Locale.getDefault());
-		frbB.keySet().forEach(System.out::println);
-		System.out.println();
-		/*System.out.println(frbB);
-		System.out.println(frbB.getObject("12"));
-		System.out.println(frbB.getString("56"));*/
-
-		ResourceBundle crbA = ResourceBundle.getBundle("pkgs.pkg1.CBundleA", Locale.getDefault());
-		crbA.keySet().forEach(System.out::println);
-		System.out.println();
-		/*System.out.println(crbA.getObject("12"));*/
-		//System.out.println(crbA.getString("12"));//ClassCastException: java.lang.Integer cannot be cast to java.lang.String
-
-		ResourceBundle crbB = ResourceBundle.getBundle("pkgs.pkg1.CBundleB", Locale.getDefault());
-		//crbB.keySet().forEach(System.out::println);//ClassCastException: java.lang.Integer cannot be cast to java.lang.String
-		System.out.println();
-		//System.out.println(crbB.getObject("12"));//ClassCastException: java.lang.Integer cannot be cast to java.lang.String
-		//System.out.println(crbB.getString("12"));//ClassCastException: java.lang.Integer cannot be cast to java.lang.String
-		//System.out.println(crbB.getString(12));//compilation problem: //The method getString(String) in the type ResourceBundle is not applicable for the arguments (int)
-		//System.out.println(crbB.getObject(12));//compilation problem: //The method getObject(String) in the type ResourceBundle is not applicable for the arguments (int)
-
-		ResourceBundle crbC = ResourceBundle.getBundle("pkgs.pkg1.CBundleC", Locale.getDefault());
-		crbC.keySet().forEach(System.out::println);
-		System.out.println();
-		/*System.out.println(crbC.getObject("12"));
-		System.out.println(crbC.getString("56"));*/
-
-		ResourceBundle crbD = ResourceBundle.getBundle("pkgs.pkg1.CBundleD", Locale.getDefault());
-		//crbD.keySet().forEach(System.out::println);//ClassCastException: java.lang.Integer cannot be cast to java.lang.String
-		System.out.println();
-		//System.out.println(crbD.getObject("12"));//ClassCastException: java.lang.Integer cannot be cast to java.lang.String
-		//System.out.println(crbD.getString("56"));//ClassCastException: java.lang.Integer cannot be cast to java.lang.String
+		ConcurrentMap<Integer, String> cm = new ConcurrentHashMap<>();
+		cm.put(1, "qwe");
+		cm.put(2, "qwe");
+		cm.put(3, "qwe");
+		cm.put(4, "zaq");
+		cm.put(5, "xsw");
+		cm.put(6, "cde");
+		System.out.println("[cm="+(cm)+"]");
+		System.out.println("[cm="+(cm.remove(2))+"]");
+		System.out.println("[cm="+(cm)+"]");
+		System.out.println("[cm="+(cm.remove(3, "qwee"))+"]");
+		System.out.println("[cm="+(cm)+"]");
+		System.out.println("[cm="+(cm.remove(33, "qwe"))+"]");
+		System.out.println("[cm="+(cm)+"]");
+		System.out.println("[cm="+(cm.remove(55, "xsw"))+"]");
+		System.out.println("[cm="+(cm)+"]");
+		System.out.println("[cm="+(cm.remove(5, "xsww"))+"]");
+		System.out.println("[cm="+(cm)+"]");
+		System.out.println("[cm="+(cm.remove(3, "qwe"))+"]");
+		System.out.println("[cm="+(cm)+"]");
 
 	}
 
