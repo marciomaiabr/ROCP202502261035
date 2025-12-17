caad34a53970a40648a6fab43bb03905505653ab
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-17 08:09:01 -0400

    remembering PrintWriter pode atuar como low-level or high-level stream

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,18 +1,25 @@
 package pkgs.pkgExes;
 
-import java.io.FileReader;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 
 		try {
-			FileReader o = new FileReader("C:\\temp\\f202512170748.txt");
-			String s = "";
-			int r = 0;
-			while((r = o.read())>=0)
-				s+=(char)r;
-			System.out.println(s);
+			PrintWriter o = new PrintWriter("C:\\temp\\f202512170754.txt");
+			o.write("c202512170754");
+			o.flush();
+			o.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			PrintWriter o = new PrintWriter(new FileOutputStream("C:\\temp\\f202512170755.txt"));
+			o.write("cf202512170755");
+			o.flush();
 			o.close();
 		} catch (Exception e) {
 			e.printStackTrace();
