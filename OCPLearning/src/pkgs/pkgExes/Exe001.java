6023ddb722026e79e99a41683242e07bd6d608a4
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-17 07:52:34 -0400

    remembering FileReader is low-level stream

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,15 +1,18 @@
 package pkgs.pkgExes;
 
-import java.io.FileOutputStream;
+import java.io.FileReader;
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 
 		try {
-			FileOutputStream o = new FileOutputStream("C:\\temp\\f202512170746.txt");
-			o.write("c202512170746".getBytes());
-			o.flush();
+			FileReader o = new FileReader("C:\\temp\\f202512170748.txt");
+			String s = "";
+			int r = 0;
+			while((r = o.read())>=0)
+				s+=(char)r;
+			System.out.println(s);
 			o.close();
 		} catch (Exception e) {
 			e.printStackTrace();
