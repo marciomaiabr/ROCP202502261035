0ceae7a5fa2990159cd8cabf1753c7d5a3477623
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-17 07:31:48 -0400

    remembering multi-catch só pode usar 1 um pipe "|"

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,27 +1,20 @@
 package pkgs.pkgExes;
 
+import java.io.IOException;
 import java.sql.SQLException;
-
-@FunctionalInterface
-interface FunctionalInterface1 { String mA(int i1) throws SQLException ; }
-
-@FunctionalInterface
-interface FunctionalInterface2 { void mB(int i2); }
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 
-		new Exe001().go();
+		try {
+			qqc();
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
-	private void go() {
-		mqqc(v->{return (v*2)+"";});
-	}
-
-	private void mqqc(FunctionalInterface1 ff1) {System.out.println("Exe001.mqqcA()");}
-
-	private void mqqc(FunctionalInterface2 ff2) {System.out.println("Exe001.mqqcB()");}
+	static void qqc() throws SQLException , IOException {}
 
 }
