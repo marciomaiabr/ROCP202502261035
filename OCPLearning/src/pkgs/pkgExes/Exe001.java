54b34f352d88651b3cacd0907382b857eedba008
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-17 08:32:59 -0400

    remembering Files.size

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,61 +1,14 @@
 package pkgs.pkgExes;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-
-class Pessoa implements Serializable {
-	private static final long serialVersionUID = 202512170815L;
-	private Long id;
-	private String nome;
-	public Pessoa() {
-		super();
-	}
-	public Pessoa(Long id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	@Override
-	public String toString() {
-		return "Pessoa [id=" + id + ", nome=" + nome + "]";
-	}
-}
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 
 		try {
-			Pessoa pessoa = new Pessoa(7l, "Marcio");
-			System.out.println(pessoa);
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("C:\\temp\\f202512170820.pessoa"));
-			oos.writeObject(pessoa);
-			oos.flush();
-			oos.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:\\temp\\f202512170820.pessoa"));
-			Pessoa pessoa = (Pessoa) ois.readObject();
-			ois.close();
-			System.out.println(pessoa);
+			System.out.println(Files.size(Paths.get("c:", "temp", "fqqc.txt")));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
