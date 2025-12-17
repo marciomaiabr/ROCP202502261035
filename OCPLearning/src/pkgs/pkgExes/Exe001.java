3257d46278594da024f1ff666f1f76e69a28bca2
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-17 09:35:04 -0400

    remembering não confundir as classes Properties e ResourceBundle

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,12 +1,34 @@
 package pkgs.pkgExes;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 
-		System.out.println(new File("C:\\temp\\fqqc.txt").length());
+		try {
+			Properties props = new Properties();
+			props.load(new FileInputStream("C:\\Users\\Administrador\\git\\ROCP202502261035\\OCPLearning\\lib\\qqc.properties"));
+			System.out.print(props.getProperty("mystery"));
+			System.out.print(" ");
+			System.out.print(props.getProperty("more"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+
+		try {
+			ResourceBundle rb = ResourceBundle.getBundle("qqc");
+			System.out.println(rb);
+			System.out.println(rb.getObject("mystery"));
+			//System.out.println(rb.getObject("more"));//MissingResourceException: Can't find resource for bundle java.util.PropertyResourceBundle, key more
+			//System.out.println(rb.getString("more"));//MissingResourceException: Can't find resource for bundle java.util.PropertyResourceBundle, key more
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
