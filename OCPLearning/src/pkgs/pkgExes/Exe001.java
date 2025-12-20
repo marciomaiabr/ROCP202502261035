1a84b3950194218d26d341d67a81cc0c788f165f
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-20 10:55:34 -0400

    remembering Path

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,14 +1,26 @@
 package pkgs.pkgExes;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Exe001 {
 
 	public static void main(String[] args) {
 
-		Integer o1 = null;
-		System.out.println(o1 instanceof Integer);
+		Path path = Paths.get("C:\\temp\\f202512201045.txt");
+		System.out.println("[path="+(path)+"]");
 
-		Integer o2 = 7;
-		System.out.println(o2 instanceof Integer);
+		System.out.println();
+
+		Path path0 = path.getName(0);
+		System.out.println("[path0="+(path0)+"]"+"");
+		System.out.println("[path0.toAbsolutePath()="+(path0.toAbsolutePath())+"]"+"");
+
+		System.out.println();
+
+		Path path1 = path.getName(1);
+		System.out.println("[path1="+(path1)+"]"+"");
+		System.out.println("[path1.toAbsolutePath()="+(path1.toAbsolutePath())+"]"+"");
 
 	}
 
