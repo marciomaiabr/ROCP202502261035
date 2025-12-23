9df9177b2efd8b783000c033f05705a50ba65d01
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-23 11:53:09 -0400

    remembering POO variable instance

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,22 +1,38 @@
 package pkgs.pkgExes;
 
-public class Exe001 {
+class ClassA {
+	String s = "A";
+}
+
+public class Exe001 extends ClassA {
+
+	String s = "E";
 
 	public static void main(String[] args) {
 
-		Integer o1 = null;
-		System.out.println(o1 instanceof Integer);
+		ClassA tipo_ClassA_instance_ClassA = new ClassA();
+		System.out.println("[tipo_ClassA_instance_ClassA="+(tipo_ClassA_instance_ClassA.s)+"]");
+		ClassA tipo_ClassA_instance_Exe001 = new Exe001();
+		System.out.println("[tipo_ClassA_instance_Exe001="+(tipo_ClassA_instance_Exe001.s)+"]"+"[((Exe001)tipo_ClassA_instance_Exe001).s="+(((Exe001)tipo_ClassA_instance_Exe001).s)+"]");
+		ClassA tipo_ClassA_instance_ClassAA = new ClassAA();
+		System.out.println("[tipo_ClassA_instance_ClassAA="+(tipo_ClassA_instance_ClassAA.s)+"]"+"[((ClassAA)tipo_ClassA_instance_ClassAA).s="+(((ClassAA)tipo_ClassA_instance_ClassAA).s)+"]");
 
-		Integer o11 = null;
-		System.out.println(o11 == o1);
+		System.out.println();
 
-		String o111 = null;
-		//System.out.println(o111 == o1);//compilation problem:Incompatible operand types String and Integer
-		System.out.println(((Object)o111) == ((Object)o1));
+		Exe001 tipo_Exe001_instance_Exe001 = new Exe001();
+		System.out.println("[tipo_Exe001_instance_Exe001="+(tipo_Exe001_instance_Exe001.s)+"]");
+		Exe001 tipo_Exe001_instance_ClassAA = new ClassAA();
+		System.out.println("[tipo_Exe001_instance_ClassAA="+(tipo_Exe001_instance_ClassAA.s)+"]"+"[((ClassAA)tipo_Exe001_instance_ClassAA).s="+(((ClassAA)tipo_Exe001_instance_ClassAA).s)+"]"+"[((ClassA)tipo_Exe001_instance_ClassAA).s="+(((ClassA)tipo_Exe001_instance_ClassAA).s)+"]");
 
-		Integer o2 = 7;
-		System.out.println(o2 instanceof Integer);
+		System.out.println();
+
+		ClassAA tipo_ClassAA_instance_ClassAA = new ClassAA();
+		System.out.println("[tipo_ClassAA_instance_ClassAA="+(tipo_ClassAA_instance_ClassAA.s)+"]"+"[((Exe001)tipo_ClassAA_instance_ClassAA).s="+(((Exe001)tipo_ClassAA_instance_ClassAA).s)+"]"+"[((ClassA)tipo_ClassAA_instance_ClassAA).s="+(((ClassA)tipo_ClassAA_instance_ClassAA).s)+"]");
 
 	}
 
+}
+
+class ClassAA extends Exe001 {
+	String s = "AA";
 }
