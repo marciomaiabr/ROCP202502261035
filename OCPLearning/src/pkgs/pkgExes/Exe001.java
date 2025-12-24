377b901180815413371ea64a1dcfdc705667abf9
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-24 08:07:36 -0400

    testing clone method without implements Cloneable

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,38 +1,22 @@
 package pkgs.pkgExes;
 
 class ClassA {
-	String s = "A";
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 }
 
-public class Exe001 extends ClassA {
+public class Exe001 {
 
-	String s = "E";
+	ClassA classA;
 
 	public static void main(String[] args) {
 
-		ClassA tipo_ClassA_instance_ClassA = new ClassA();
-		System.out.println("[tipo_ClassA_instance_ClassA="+(tipo_ClassA_instance_ClassA.s)+"]");
-		ClassA tipo_ClassA_instance_Exe001 = new Exe001();
-		System.out.println("[tipo_ClassA_instance_Exe001="+(tipo_ClassA_instance_Exe001.s)+"]"+"[((Exe001)tipo_ClassA_instance_Exe001).s="+(((Exe001)tipo_ClassA_instance_Exe001).s)+"]");
-		ClassA tipo_ClassA_instance_ClassAA = new ClassAA();
-		System.out.println("[tipo_ClassA_instance_ClassAA="+(tipo_ClassA_instance_ClassAA.s)+"]"+"[((ClassAA)tipo_ClassA_instance_ClassAA).s="+(((ClassAA)tipo_ClassA_instance_ClassAA).s)+"]");
-
-		System.out.println();
-
-		Exe001 tipo_Exe001_instance_Exe001 = new Exe001();
-		System.out.println("[tipo_Exe001_instance_Exe001="+(tipo_Exe001_instance_Exe001.s)+"]");
-		Exe001 tipo_Exe001_instance_ClassAA = new ClassAA();
-		System.out.println("[tipo_Exe001_instance_ClassAA="+(tipo_Exe001_instance_ClassAA.s)+"]"+"[((ClassAA)tipo_Exe001_instance_ClassAA).s="+(((ClassAA)tipo_Exe001_instance_ClassAA).s)+"]"+"[((ClassA)tipo_Exe001_instance_ClassAA).s="+(((ClassA)tipo_Exe001_instance_ClassAA).s)+"]");
-
-		System.out.println();
-
-		ClassAA tipo_ClassAA_instance_ClassAA = new ClassAA();
-		System.out.println("[tipo_ClassAA_instance_ClassAA="+(tipo_ClassAA_instance_ClassAA.s)+"]"+"[((Exe001)tipo_ClassAA_instance_ClassAA).s="+(((Exe001)tipo_ClassAA_instance_ClassAA).s)+"]"+"[((ClassA)tipo_ClassAA_instance_ClassAA).s="+(((ClassA)tipo_ClassAA_instance_ClassAA).s)+"]");
-
 	}
 
-}
+	void m(ClassA classA) throws Exception {
+		this.classA = (ClassA) classA.clone();
+	}
 
-class ClassAA extends Exe001 {
-	String s = "AA";
 }
