17f26c2f738284b6f940c9131de98fd2edcb1560
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-25 09:31:43 -0400

    remembering enum

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,20 +1,31 @@
 package pkgs.pkgExes;
 
-enum DIAS_SEMANAS{
+interface InterfaceA {}
+
+class ClassA {
+
+}
+
+//enum DIAS_SEMANAS extends ClassA {//Syntax error on token "extends", implements expected
+enum DIAS_SEMANAS implements InterfaceA {
 	SEG{
 		@Override
-		void mq() {}
+		void m2() {}
 	},  
 	TER{
 		@Override
-		void mq() {}
+		void m2() {}
 	},
 	QUAR{
 		@Override
-		void mq() {}
+		void m2() {}
 	};
-	abstract void mq();
+	abstract void m2();
 }
+
+//class ClassAA extends DIAS_SEMANAS {}//The type DIAS_SEMANAS cannot be the superclass of ClassAA; a superclass must be a class
+//class ClassAA extends DIAS_SEMANAS.TER {}//DIAS_SEMANAS.TER cannot be resolved to a type
+//class ClassAA extends TER {}//TER cannot be resolved to a type
 
 public class Exe001 {
 
