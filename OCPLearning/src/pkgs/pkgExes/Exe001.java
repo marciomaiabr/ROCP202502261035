cb1650ca838c3d7b9a1d656dea0c345dfa1e7ff9
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-25 11:42:52 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,22 +1,10 @@
 package pkgs.pkgExes;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Exe001 {
 
 	public static void main(String[] args) {
 
-		Queue<Integer> d = new ArrayDeque<>();
-		d.offer(18);
-		d.offer(5);
-		System.out.println(d);
-		//d.push(13);//The method push(int) is undefined for the type Queue<Integer>
-		d.add(10);
-		d.add(10);
-		System.out.println(d);
-		while(!d.isEmpty())
-			System.out.println(d.poll());
+
 
 	}
 
