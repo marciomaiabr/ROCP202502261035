bb68266663bee81a439c7dd6f1363dd6d97bed43
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-25 09:40:08 -0400

    remembering enum with case

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,37 +1,21 @@
 package pkgs.pkgExes;
 
-interface InterfaceA {}
-
-class ClassA {
-
+enum DIAS_SEMANAS {
+	SEG, TER, QUAR
 }
-
-//enum DIAS_SEMANAS extends ClassA {//Syntax error on token "extends", implements expected
-enum DIAS_SEMANAS implements InterfaceA {
-	SEG{
-		@Override
-		void m2() {}
-	},  
-	TER{
-		@Override
-		void m2() {}
-	},
-	QUAR{
-		@Override
-		void m2() {}
-	};
-	abstract void m2();
-}
-
-//class ClassAA extends DIAS_SEMANAS {}//The type DIAS_SEMANAS cannot be the superclass of ClassAA; a superclass must be a class
-//class ClassAA extends DIAS_SEMANAS.TER {}//DIAS_SEMANAS.TER cannot be resolved to a type
-//class ClassAA extends TER {}//TER cannot be resolved to a type
 
 public class Exe001 {
 
 	public static void main(String[] args) {
 
+		DIAS_SEMANAS d = DIAS_SEMANAS.SEG;
 
+		switch (d) {
+		case SEG:
+			break;
+		default:
+			break;
+		}
 
 	}
 
