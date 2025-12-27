e44960066f09686a9531f68154d31b5f3560499e
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-27 10:16:10 -0400

    remembering principals functional interfaces full

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -10,11 +10,34 @@ public class Exe001 {
 
 		Predicate p;
 		BiPredicate bp;
-		//CharSupplier c;//CharSupplier cannot be resolved to a type//Remember, Java only includes primitive functional interfaces that operate on double, int, or long. 
-		LongFunction l;
+		//CharSupplier c;//CharSupplier cannot be resolved to a type//Remember, Java only includes primitive functional interfaces that operate on double, int, or long. boolean have only Supplier
+		BooleanSupplier bs;
+		DoubleSupplier ds;
+		IntSupplier is;
+		LongSupplier ls;
+		//BooleanBinaryOperator bbo;//BooleanBinaryOperator cannot be resolved to a type
+		DoubleBinaryOperator dbo;
+		IntBinaryOperator ibo;
+		LongBinaryOperator lbo;
+		//BooleanUnaryOperator buo;//BooleanUnaryOperator cannot be resolved to a type
+		DoubleUnaryOperator duo;
+		IntUnaryOperator iuo;
+		LongUnaryOperator luo;
+		//BooleanPredicate bp;//BooleanPredicate cannot be resolved to a type
+		DoublePredicate dp;
+		IntPredicate ip;
+		LongPredicate lp;
+		//BooleanConsumer bc;//BooleanConsumer cannot be resolved to a type
+		DoubleConsumer dc;
+		IntConsumer ic;
+		LongConsumer lc;
+		//BooleanFunction bf;//BooleanFunction cannot be resolved to a type
+		DoubleFunction df;
+		IntFunction inf;
+		LongFunction lf;
 		UnaryOperator u;
 		Function f;
-		BiFunction bf;
+		BiFunction bif;
 		BinaryOperator bi;
 
 	}
