8b10083da3fd0d3c63389d08fe3264f3442c0341
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-27 09:58:22 -0400

    remembering principals functional interfaces

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,19 +1,21 @@
 package pkgs.pkgExes;
 
-class ClassA implements AutoCloseable {
-
-	//@Override public void close() throws Exception { throw new Exception("ClassA... close()... Exception..."); }
-	@Override public void close() { throw new RuntimeException("ClassA... close()... RuntimeException..."); }
-	
-}
+import java.util.function.*;
 
 public class Exe001 {
 
+
+
 	public static void main(String[] args) {
 
-		try(ClassA ca = new ClassA();){
-			System.out.println(ca);
-		}
+		Predicate p;
+		BiPredicate bp;
+		//CharSupplier c;//CharSupplier cannot be resolved to a type//Remember, Java only includes primitive functional interfaces that operate on double, int, or long. 
+		LongFunction l;
+		UnaryOperator u;
+		Function f;
+		BiFunction bf;
+		BinaryOperator bi;
 
 	}
 
