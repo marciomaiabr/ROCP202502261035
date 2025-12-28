30ee56789a278cdbfe10d9704746bb39c5914fa7
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-28 08:31:47 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,6 @@
 package pkgs.pkgExes;
 
-import java.util.function.*;
+
 
 public class Exe001 {
 
@@ -8,37 +8,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		Predicate p;
-		BiPredicate bp;
-		//CharSupplier c;//CharSupplier cannot be resolved to a type//Remember, Java only includes primitive functional interfaces that operate on double, int, or long. boolean have only Supplier
-		BooleanSupplier bs;
-		DoubleSupplier ds;
-		IntSupplier is;
-		LongSupplier ls;
-		//BooleanBinaryOperator bbo;//BooleanBinaryOperator cannot be resolved to a type
-		DoubleBinaryOperator dbo;
-		IntBinaryOperator ibo;
-		LongBinaryOperator lbo;
-		//BooleanUnaryOperator buo;//BooleanUnaryOperator cannot be resolved to a type
-		DoubleUnaryOperator duo;
-		IntUnaryOperator iuo;
-		LongUnaryOperator luo;
-		//BooleanPredicate bp;//BooleanPredicate cannot be resolved to a type
-		DoublePredicate dp;
-		IntPredicate ip;
-		LongPredicate lp;
-		//BooleanConsumer bc;//BooleanConsumer cannot be resolved to a type
-		DoubleConsumer dc;
-		IntConsumer ic;
-		LongConsumer lc;
-		//BooleanFunction bf;//BooleanFunction cannot be resolved to a type
-		DoubleFunction df;
-		IntFunction inf;
-		LongFunction lf;
-		UnaryOperator u;
-		Function f;
-		BiFunction bif;
-		BinaryOperator bi;
+
 
 	}
 
