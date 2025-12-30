8878cd69dbdb31733253de6d04887da5052bef62
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-30 07:43:45 -0400

    remembering LocalDate Duration Period

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,9 @@
 package pkgs.pkgExes;
 
-
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 
 public class Exe001 {
 
@@ -8,7 +11,13 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-
+		LocalDate montyPythonDay = LocalDate.of(2017, Month.MAY, 10);
+		LocalDate aprilFools = LocalDate.of(2018, Month.APRIL, 1);
+		Duration duration = Duration.ofDays(1);
+		Period period = Period.ofDays(1);
+		//LocalDate result = montyPythonDay.minus(duration);//UnsupportedTemporalTypeException: Unsupported unit: Seconds
+		LocalDate result = montyPythonDay.minus(period);
+		System.out.println(result + " " + aprilFools.isBefore(result));
 
 	}
 
