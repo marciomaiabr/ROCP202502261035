c246a64eade76aa4ae5e3a63809f959654982d5b
Administrador <Administrador@MMW10LAB.mshome.net>
2025-12-30 09:14:30 -0400

    remembering Connection Statement ResultSet extends AutoCloseable

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,9 @@
 package pkgs.pkgExes;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Exe001 {
 
@@ -8,9 +11,14 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		try {
-			Class.forName("qqc");//ClassNotFoundException: qqc
-		} catch (Exception e) {
+		try (
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ocp202509020748", "root", "senha123");
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("select count(*) from pessoa")
+			) {
+			if(rs.next())
+				System.out.println(rs.getInt(1));
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 
