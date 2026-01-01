8185e3147f02d1ee6f61299d0640f2ef3d003b46
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-01 10:01:41 -0400

    remembering OutputStream Writer

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,9 +1,9 @@
 package pkgs.pkgExes;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.io.Writer;
 
 public class Exe001 {
 
@@ -11,14 +11,15 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		try (
-				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ocp202509020748", "root", "senha123");
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery("select count(*) from pessoa")
-			) {
-			if(rs.next())
-				System.out.println(rs.getInt(1));
-		}catch (Exception e) {
+		try (OutputStream os = new FileOutputStream("C:\\temp\\f202601010955.txt");) {
+			os.write("c202601010955".getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try (Writer w = new FileWriter("C:\\temp\\f202601010959.txt");) {
+			w.write("c202601010959");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
