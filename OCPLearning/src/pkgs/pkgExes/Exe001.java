fc1a414a095d70c530340cf3564bdb2e307e179b
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-04 10:55:14 -0400

    remembering Locale ResourceBundle

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,7 @@
 package pkgs.pkgExes;
 
-import java.time.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Exe001 {
 
@@ -8,47 +9,17 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		LocalTime o3 = null;
+		{
+			Locale.setDefault(new Locale("en", "UK"));
+			ResourceBundle rb = ResourceBundle.getBundle("Train", new Locale("en", "UK"));
+			System.out.println(rb.getString("ride") + " " + rb.getString("train"));
+		}
 
-		//o3 = LocalTime.from(LocalDate.now());//DateTimeException: Unable to obtain LocalTime from TemporalAccessor: 2026-01-04 of type java.time.LocalDate
-		System.out.println(o3);
-
-		o3 = LocalTime.from(LocalTime.now());
-		System.out.println(o3);
-
-		o3 = LocalTime.from(LocalDateTime.now());
-		System.out.println(o3);
-
-		o3 = LocalTime.from(LocalDateTime.now().atZone(ZoneId.systemDefault()));
-		System.out.println(o3);
-
-		LocalDate o2 = null;
-
-		o2 = LocalDate.from(LocalDate.now());
-		System.out.println(o2);
-
-		//o2 = LocalDate.from(LocalTime.now());//DateTimeException: Unable to obtain LocalDate from TemporalAccessor: 10:18:19.997 of type java.time.LocalTime
-		System.out.println(o2);
-
-		o2 = LocalDate.from(LocalDateTime.now());
-		System.out.println(o2);
-
-		o2 = LocalDate.from(LocalDateTime.now().atZone(ZoneId.systemDefault()));
-		System.out.println(o2);
-
-		LocalDateTime o = null;
-
-		//o = LocalDateTime.from(LocalDate.now());//DateTimeException: Unable to obtain LocalTime from TemporalAccessor: 2026-01-04 of type java.time.LocalDate
-		System.out.println(o);
-
-		//o = LocalDateTime.from(LocalTime.now());//DateTimeException: Unable to obtain LocalDateTime from TemporalAccessor: 10:15:54.356 of type java.time.LocalTime
-		System.out.println(o);
-
-		o = LocalDateTime.from(LocalDateTime.now());
-		System.out.println(o);
-
-		o = LocalDateTime.from(LocalDateTime.now().atZone(ZoneId.systemDefault()));
-		System.out.println(o);
+		{
+			Locale.setDefault(new Locale("en", "US"));
+			ResourceBundle rb = ResourceBundle.getBundle("Train", new Locale("en", "US"));
+			System.out.println(rb.getString("ride") + " " + rb.getString("train"));
+		}
 
 	}
 
