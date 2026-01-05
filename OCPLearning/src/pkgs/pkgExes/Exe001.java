4ac1c1a83ed802417736c96136c0d340e48be7cc
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-05 10:39:15 -0400

    remembering Locale ResourceBundle

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,7 @@
 package pkgs.pkgExes;
 
-
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Exe001 {
 
@@ -8,7 +9,32 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
+		try{
+			System.out.println("[Locale.getDefault()="+(Locale.getDefault())+"]");
+			ResourceBundle rb = ResourceBundle.getBundle("R1");
+			System.out.println("[rb="+(rb)+"]"+"[rb.getLocale()="+(rb.getLocale())+"]");
+			System.out.println(rb.getString("A1"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 
+		System.out.println();
+
+		//Locale.setDefault(new Locale(null));//NullPointerException
+		//Locale.setDefault(null);//NullPointerException: Can't set default locale to NULL
+		//Locale.setDefault(new Locale("", null));//NullPointerException
+		//Locale.setDefault(new Locale(null, ""));//NullPointerException
+		//Locale.setDefault(new Locale(null, null));//NullPointerException
+		Locale.setDefault(new Locale("", ""));
+		System.out.println("[Locale.getDefault()="+(Locale.getDefault())+"]");
+
+		try{
+			ResourceBundle rb = ResourceBundle.getBundle("R1");
+			System.out.println("[rb="+(rb)+"]"+"[rb="+(rb.getLocale())+"]");
+			System.out.println(rb.getString("A1"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 
 	}
 
