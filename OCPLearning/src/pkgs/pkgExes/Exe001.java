a2566af7a063c2f40e13e27bc039f2297a0ce5d4
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-05 11:14:17 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,7 +1,6 @@
 package pkgs.pkgExes;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+
 
 public class Exe001 {
 
@@ -9,16 +8,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		try{
-			//Locale.setDefault(new Locale("fr"));
-			System.out.println("[Locale.getDefault()="+(Locale.getDefault())+"]");
-			//ResourceBundle rb = ResourceBundle.getBundle("R1" , new Locale("it") );//MissingResourceException: Can't find bundle for base name R1, locale it
-			ResourceBundle rb = ResourceBundle.getBundle("R1");
-			System.out.println("[rb="+(rb)+"]"+"[rb="+(rb.getLocale())+"]");
-			System.out.println(rb.getString("A1"));
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+
 
 	}
 
