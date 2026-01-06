b6ebd53fa61076f25d154051f0da66ff203c2374
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-06 08:37:08 -0400

    remembering OffsetDateTime

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,7 @@
 package pkgs.pkgExes;
 
-
+import java.time.*;
+import java.time.temporal.ChronoField;
 
 public class Exe001 {
 
@@ -8,7 +9,27 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
+		System.out.println();
+		LocalDateTime ldt200304060100 = LocalDateTime.of(2003, Month.APRIL, 6, 1, 0);
+		System.out.println("[ldt200304060100="+(ldt200304060100)+"]");
+		OffsetDateTime odt200304060100 = OffsetDateTime.of(ldt200304060100, ZoneOffset.of("-05:00"));
+		System.out.println("[odt200304060100="+(odt200304060100)+"]");
 
+		System.out.println();
+		LocalDateTime ldt200304060200 = LocalDateTime.of(2003, Month.APRIL, 6, 2, 0);
+		System.out.println("[ldt200304060200="+(ldt200304060200)+"]");
+		OffsetDateTime odt200304060200 = OffsetDateTime.of(ldt200304060200, ZoneOffset.of("-05:00"));
+		System.out.println("[odt200304060200="+(odt200304060200)+"]");
+		odt200304060200 = odt200304060200.with(ChronoField.HOUR_OF_DAY, 5);
+		System.out.println("[odt200304060200="+(odt200304060200)+"]");
+		odt200304060200 = odt200304060200.with(ChronoField.HOUR_OF_DAY, 2);
+		System.out.println("[odt200304060200="+(odt200304060200)+"]");
+
+		System.out.println();
+		LocalDateTime ldt200304060300 = LocalDateTime.of(2003, Month.APRIL, 6, 3, 0);
+		System.out.println("[ldt200304060300="+(ldt200304060300)+"]");
+		OffsetDateTime odt200304060300 = OffsetDateTime.of(ldt200304060300, ZoneOffset.of("-05:00"));
+		System.out.println("[odt200304060300="+(odt200304060300)+"]");
 
 	}
 
