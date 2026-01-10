6e7b28821cd2d0e43267c3df5e7c90eea3556444
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-10 09:41:33 -0400

    remembering Set HashSet TreeSet

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,6 @@
 package pkgs.pkgExes;
 
-
+import java.util.*;
 
 public class Exe001 {
 
@@ -8,7 +8,19 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
+		Set set = null;
 
+		set = new HashSet<>();
+		set.add("2");
+		set.add(3);
+		set.add("1");
+		set.forEach(System.out::println);
+
+		set = new TreeSet<>();
+		set.add("2");
+		//set.add(3);//ClassCastException: java.lang.String cannot be cast to java.lang.Integer
+		set.add("1");
+		set.forEach(System.out::println);
 
 	}
 
