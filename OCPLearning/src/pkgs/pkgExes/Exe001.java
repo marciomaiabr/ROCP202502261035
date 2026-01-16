eef2235e7e52b2fdf54a8953838d4ea57002684d
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-16 08:27:01 -0400

    remembering generics exercise 8

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,6 @@
 package pkgs.pkgExes;
 
-
+import java.util.*;
 
 public class Exe001 {
 
@@ -8,10 +8,34 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
+		ArrayList input = null;
+		List output = null;
 
+		//ArrayList<Integer> input = null;
+		//List<Integer> output = null;
+
+		//ArrayList<Integer> input = null;
+		//List<Number> output = null;
+
+		//ArrayList<Number> input = null;
+		//List<Integer> output = null;
+
+		//List<Number> input = null;
+		//List<Number> output = null;
+
+		//List<Integer> input = null;
+		//List<Integer> output = null;
+
+		input = null;
+		output = process(input);
+		System.out.println(input);
+		System.out.println(output);
 
 	}
 
-
+	public static <E extends Number> List<E> process(List<E> nums){ return null; }
+	//public static List process(List nums){ return null; }
+	//public static <E extends Number> List<E> process(List nums){ return null; }
+	//public static <E extends Number> List process(List<E> nums){ return null; }
 
 }
