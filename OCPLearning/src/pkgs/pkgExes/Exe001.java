bc211076d000e84c28b1c13998e92dfd53e9519b
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-16 08:23:50 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,6 @@
 package pkgs.pkgExes;
 
-import java.util.*;
+
 
 public class Exe001 {
 
@@ -8,29 +8,10 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		//ArrayList<Integer> input = null;
-		//List<Integer> output = null;
 
-		//ArrayList<Integer> input = null;
-		//List<Number> output = null;
-
-		//ArrayList<Number> input = null;
-		//List<Integer> output = null;
-
-		//List<Number> input = null;
-		//List<Number> output = null;
-
-		//List<Integer> input = null;
-		//List<Integer> output = null;
-
-		input = null;
-		output = process(input);
-		System.out.println(input);
-		System.out.println(output);
 
 	}
 
-	public static <E extends Number> List<E> process(List<E> nums){ return null; }
-	//public static List process(List nums){ return null; }
+
 
 }
