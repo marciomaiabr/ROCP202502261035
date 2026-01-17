2202795e91b6fcaee4e0463c3872697cb559b3fb
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-17 08:19:18 -0400

    remembering inner class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,23 +1,35 @@
 package pkgs.pkgExes;
 
-class ClassMM {
-	int level = 0;
-	@Override public String toString() { return "[ClassMM][level="+(level)+"]"; }
-}
+
 
 public class Exe001 {
 
-	class ClassMM {
-		int level = 1;
-		@Override public String toString() { return "[ClassMM][level="+(level)+"]"; }
+	class ClassA {
+		int level = 0;
+		public String letra = "A";
+		@Override public String toString() { return "[ClassA][level="+(level)+"]"; }
+		//public String getClassBthis() { return ClassB.this.toString(); }//No enclosing instance of the type Exe001.ClassA.ClassB is accessible in scope
+		//public String getClassAClassBthis() { return ClassA.ClassB.this.toString(); }//No enclosing instance of the type Exe001.ClassA.ClassB is accessible in scope
+		public String getClassAthis() { return ClassA.this.toString(); }
+		class ClassB {
+			int level = 1;
+			public String letra = "B";
+			@Override public String toString() { return "[ClassB][level="+(level)+"]"; }
+			public String getClassBthis() { return ClassB.this.toString(); }
+			public String getClassAClassBthis() { return ClassA.ClassB.this.toString(); }
+			public String getClassAthis() { return ClassA.this.toString(); }
+			class ClassC {
+				int level = 2;
+				public String letra = "C";
+				@Override public String toString() { return "[ClassC][level="+(level)+"]"; }
+				public String getClassBthis() { return ClassB.this.toString(); }
+				public String getClassAClassBthis() { return ClassA.ClassB.this.toString(); }
+				public String getClassAthis() { return ClassA.this.toString(); }
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-
-		class ClassMM {
-			int level = 2;
-			@Override public String toString() { return "[ClassMM][1][level="+(level)+"]"; }
-		}
 
 		new Exe001().go();
 
@@ -25,13 +37,12 @@ public class Exe001 {
 
 	void go() {
 
-		class ClassMM {
-			int level = 2;
-			@Override public String toString() { return "[ClassMM][2][level="+(level)+"]"; }
-		}
-
-		ClassMM a = new ClassMM();
-		System.out.println(a);
+		ClassA ca = new ClassA();
+		System.out.println("[ca="+(ca)+"]"+"[ca.letra="+(ca.letra)+"]");
+		ClassA.ClassB cb = ca.new ClassB();
+		System.out.println("[cb="+(cb)+"]"+"[cb.letra="+(cb.letra)+"]");
+		ClassA.ClassB.ClassC cc = cb.new ClassC();
+		System.out.println("[cc="+(cc)+"]"+"[cc.letra="+(cc.letra)+"]"+"[cc.getClassBthis()="+(cc.getClassBthis())+"]"+"[cc.getClassAClassBthis()="+(cc.getClassAClassBthis())+"]"+"[cc.getClassAthis()="+(cc.getClassAthis())+"]");
 
 	}
 
