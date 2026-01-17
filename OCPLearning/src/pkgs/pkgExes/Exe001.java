66ad1546308061db2b8a8eba9a3c93a38e559cae
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-17 07:38:15 -0400

    remembering inner class

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,17 +1,38 @@
 package pkgs.pkgExes;
 
-
+class ClassMM {
+	int level = 0;
+	@Override public String toString() { return "[ClassMM][level="+(level)+"]"; }
+}
 
 public class Exe001 {
 
-
+	class ClassMM {
+		int level = 1;
+		@Override public String toString() { return "[ClassMM][level="+(level)+"]"; }
+	}
 
 	public static void main(String[] args) {
 
+		class ClassMM {
+			int level = 2;
+			@Override public String toString() { return "[ClassMM][1][level="+(level)+"]"; }
+		}
 
+		new Exe001().go();
 
 	}
 
+	void go() {
 
+		class ClassMM {
+			int level = 2;
+			@Override public String toString() { return "[ClassMM][2][level="+(level)+"]"; }
+		}
+
+		ClassMM a = new ClassMM();
+		System.out.println(a);
+
+	}
 
 }
