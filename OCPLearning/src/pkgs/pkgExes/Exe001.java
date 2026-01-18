76b0bc5a84e66f2c634c006d3e33dfc18fb62ea4
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-18 11:04:37 -0400

    remembering Thread synchronized wait notify

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,10 +8,52 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-
+		new Exe001().go();
 
 	}
 
+	void go() {
 
+		Object obj = new Object();
+
+		System.out.println("[aki][1]");
+		new Thread(()->{
+			System.out.println("[aki][2]");
+			synchronized (obj) {
+				System.out.println("[aki][3]");
+				try {
+					Thread.sleep(3000);
+				} catch (Exception e) {}
+				System.out.println("[aki][3.1]");
+				try {
+					obj.wait();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println("[aki][3.2]");
+				try {
+					Thread.sleep(3000);
+				} catch (Exception e) {}
+				System.out.println("[aki][4]");
+			}
+		}).start();
+		System.out.println("[aki][5]");
+
+		System.out.println("[aki][6]");
+		new Thread(()->{
+			System.out.println("[aki][7]");
+			synchronized (obj) {
+				System.out.println("[aki][8]");
+				try {
+					obj.notify();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println("[aki][9]");
+			}
+		}).start();
+		System.out.println("[aki][10]");
+
+	}
 
 }
