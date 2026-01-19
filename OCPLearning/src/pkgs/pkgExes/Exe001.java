770f1a88066e55ef312d4119757e2055f3a6fc07
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-19 08:02:20 -0400

    remembering synchronized

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -14,45 +14,12 @@ public class Exe001 {
 
 	void go() {
 
-		Object obj = new Object();
-
-		System.out.println("[aki][1]");
-		new Thread(()->{
-			System.out.println("[aki][2]");
-			synchronized (obj) {
-				System.out.println("[aki][3]");
-				try {
-					Thread.sleep(3000);
-				} catch (Exception e) {}
-				System.out.println("[aki][3.1]");
-				try {
-					obj.wait();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				System.out.println("[aki][3.2]");
-				try {
-					Thread.sleep(3000);
-				} catch (Exception e) {}
-				System.out.println("[aki][4]");
-			}
-		}).start();
-		System.out.println("[aki][5]");
-
-		System.out.println("[aki][6]");
-		new Thread(()->{
-			System.out.println("[aki][7]");
-			synchronized (obj) {
-				System.out.println("[aki][8]");
-				try {
-					obj.notify();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				System.out.println("[aki][9]");
-			}
-		}).start();
-		System.out.println("[aki][10]");
+		Object o = null;
+		System.out.println("1...");
+		synchronized (o) {//NullPointerException
+			System.out.println("2...");
+		}
+		System.out.println("3...");
 
 	}
 
