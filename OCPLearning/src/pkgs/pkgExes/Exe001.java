bc0cee43c52360a7dd4df8cf66d8272b1bbc5643
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-22 07:53:54 -0400

    remembering ConcurrentMap ConcurrentHashMap remove

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,7 +1,7 @@
 package pkgs.pkgExes;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Exe001 {
 
@@ -9,36 +9,17 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		ArrayBlockingQueue<Integer> abq = new ArrayBlockingQueue<>(3);
-		System.out.println(abq.add(1));
-		System.out.println(abq.add(1));
-		System.out.println(abq.offer(1));
-		System.out.println(abq.size());
-		//System.out.println(abq.add(2));//IllegalStateException: Queue full
-		System.out.println(abq.offer(2));
-		try {
-			System.out.println(abq.offer(2, 3, TimeUnit.SECONDS));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		new Thread(()->{
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e) {}
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e) {}
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e) {}
-			abq.clear();
-		}).start();
-		try {
-			abq.put(2);//espera por espaço na fila/colecao
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(abq.size());
+		ConcurrentMap<String, Integer> m = new ConcurrentHashMap<>();
+		String john = "John";
+		System.out.println(m.put(john, 23));
+		System.out.println(m.size());
+		System.out.println(m);
+		System.out.println(m.remove(john, 3));
+		System.out.println(m);
+		System.out.println(m.remove("qqc", 23));
+		System.out.println(m);
+		System.out.println(m.remove(john, 23));
+		System.out.println(m);
 
 	}
 
