42ba1ab0edbf67ea805597ed4e8c986bde2c3d1b
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-23 07:42:57 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,42 +1,6 @@
 package pkgs.pkgExes;
 
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveTask;
 
-class SomaTask extends RecursiveTask<Integer> {
-
-	private int[] array;
-	private int inicio, fim;
-	private static final int LIMITE = 3;
-
-	SomaTask(int[] array, int inicio, int fim) {
-		this.array = array;
-		this.inicio = inicio;
-		this.fim = fim;
-	}
-
-	@Override
-	protected Integer compute() {
-		if (fim - inicio <= LIMITE) {
-			int soma = 0;
-			for (int i = inicio; i < fim; i++) {
-				soma += array[i];
-			}
-			return soma;
-		} else {
-			int meio = (inicio + fim) / 2;
-
-			SomaTask esquerda = new SomaTask(array, inicio, meio);
-			SomaTask direita = new SomaTask(array, meio, fim);
-
-			esquerda.fork();// executa em paralelo
-			int resultadoDireita = direita.compute(); 
-			int resultadoEsquerda = esquerda.join();
-
-			return resultadoEsquerda + resultadoDireita;
-		}
-	}
-}
 
 public class Exe001 {
 
@@ -44,8 +8,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		int [] array = {1,2,3,4,5,6,7,8,9,10};
-		System.out.println(new ForkJoinPool().invoke(new SomaTask(array, 0, array.length)));;
+
 
 	}
 
