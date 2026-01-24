9f1fed829593b3dd0cdbf9611bde772256a091b0
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-24 11:56:11 -0400

    remembering enum

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,8 @@
 package pkgs.pkgExes;
 
-
+enum DaysOff {
+	Thanksgiving, PresidentsDay, ValentinesDay
+}
 
 public class Exe001 {
 
@@ -8,7 +10,17 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-
+		final DaysOff input = DaysOff.Thanksgiving;
+		switch(input) {
+			default:
+				//break;
+			case ValentinesDay:
+				System.out.print("1");
+				//break;
+			case PresidentsDay:
+				System.out.print("2");
+				//break;
+		}
 
 	}
 
