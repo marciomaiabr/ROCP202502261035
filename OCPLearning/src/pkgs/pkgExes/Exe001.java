1bbeacbf8e057c1d1c7b737919cf02d8a02a00f6
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-26 07:38:53 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -8,27 +8,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-/*
-Um switch funciona com os tipos de dados primitivos byte, short, char e int.
-Ele também funciona com tipos enumerados, a classe String e algumas classes especiais que encapsulam certos tipos primitivos: Character, Byte, Short e Integer.
-*/
 
-		String sp = new String("");
-
-		final int i1 = new Integer(1);
-		final int i2 = new Integer(2);
-		final int i3 = new Integer(3);
-
-		switch (sp) {
-		case "1":
-			break;
-		case "2":
-			break;
-		case "3":
-			break;
-		default:
-			break;
-		}
 
 	}
 
