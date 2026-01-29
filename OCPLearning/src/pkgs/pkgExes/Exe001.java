4e2fe769cf7047ea1777a3c1290f4f015ae77ac1
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-29 07:46:47 -0400

    remembering InputStream read skip

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,7 @@
 package pkgs.pkgExes;
 
-
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class Exe001 {
 
@@ -8,7 +9,17 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-
+		try {
+			InputStream is = null;
+			is = new ByteArrayInputStream("AEIOU".getBytes());
+			System.out.println(is);
+			System.out.println((char)is.read());
+			is = new ByteArrayInputStream("AEIOU".getBytes());
+			System.out.println(is);
+			System.out.println(is.skip(10));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
