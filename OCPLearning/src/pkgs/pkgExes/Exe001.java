8d7fbc408c051bcf2300dfbfc8981ba657519ddb
Administrador <Administrador@MMW10LAB.mshome.net>
2026-01-29 07:47:58 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,7 +1,6 @@
 package pkgs.pkgExes;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+
 
 public class Exe001 {
 
@@ -9,17 +8,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		try {
-			InputStream is = null;
-			is = new ByteArrayInputStream("AEIOU".getBytes());
-			System.out.println(is);
-			System.out.println((char)is.read());
-			is = new ByteArrayInputStream("AEIOU".getBytes());
-			System.out.println(is);
-			System.out.println(is.skip(10));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
 
 	}
 
