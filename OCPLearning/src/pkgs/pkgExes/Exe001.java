3603cca7f594cea40b10bd8a93133f9b0abeb8c7
Administrador <Administrador@MMW10LAB.mshome.net>
2026-02-02 07:28:28 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,20 +1,6 @@
 package pkgs.pkgExes;
 
-import java.util.Arrays;
 
-class ClassA{
-	public ClassA(Integer i) {System.out.println("[i="+(i)+"]"+"");}
-	public ClassA() {System.out.println("[=()]"+"");}
-}
-
-class ClassB{
-	public ClassB(String s) {System.out.println("[s="+(s)+"]"+"");}
-	public ClassB() {System.out.println("[=()]"+"");}
-}
-
-class ClassC{
-	public ClassC() {System.out.println("[=()]"+"");}
-}
 
 public class Exe001 {
 
@@ -22,9 +8,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		Arrays.asList(1,2,3).forEach(ClassA::new);
-		//Arrays.asList(1,2,3).forEach(ClassB::new);//compilation problem://The type ClassB does not define ClassB(Integer) that is applicable here
-		//Arrays.asList(1,2,3).forEach(ClassC::new);//compilation problems://The method forEach(Consumer<? super Integer>) in the type Iterable<Integer> is not applicable for the arguments (ClassC::new)//The type ClassC does not define ClassC(Integer) that is applicable here
+
 
 	}
 
