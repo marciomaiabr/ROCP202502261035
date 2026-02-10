680aa7c615b447a87527ceb99a04edede72b80f1
Administrador <Administrador@MMW10LAB.mshome.net>
2026-02-10 08:02:28 -0400

    remembering enums

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,7 +1,9 @@
 package pkgs.pkgExes;
 
-import java.time.Duration;
-import java.time.Period;
+enum Color {
+RED, BLUE, ORANGE, GREEN;
+Color() {}
+}
 
 public class Exe001 {
 
@@ -9,10 +11,7 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-		Duration duration = Duration.ofDays(1);
-		Period period = Period.ofDays(1);
-		System.out.println("[duration="+(duration)+"]"+"");
-		System.out.println("[period="+(period)+"]"+"");
+		System.out.println(Color.BLUE);
 
 	}
 
