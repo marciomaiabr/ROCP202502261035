ab3bf50b8b07d4f214d2bc7f2b5524b8a7b22ce5
Administrador <Administrador@MMW10LAB.mshome.net>
2026-02-16 07:11:07 -0400

    remembering Locale

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,6 @@
 package pkgs.pkgExes;
 
-
+import java.util.Locale;
 
 public class Exe001 {
 
@@ -8,7 +8,18 @@ public class Exe001 {
 
 	public static void main(String[] args) {
 
-
+		Locale.setDefault(Locale.KOREAN);
+		System.out.println(Locale.getDefault());
+		Locale.setDefault(new Locale("en", "AU"));
+		System.out.println(Locale.getDefault());
+		Locale.setDefault(new Locale("EN"));
+		System.out.println(Locale.getDefault());
+		Locale.setDefault(new Locale("QQ"));
+		System.out.println(Locale.getDefault());
+		Locale.setDefault(new Locale("QQ", "q1"));
+		System.out.println(Locale.getDefault());
+		Locale.setDefault(Locale.FRANCE);
+		System.out.println(Locale.getDefault());
 
 	}
 
