57a502101b8fb5e2c755b163ce4f94832643ee10
Administrador <Administrador@MMW10LAB.mshome.net>
2026-03-06 09:56:24 -0400

    cleaning

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,6 @@
 package pkgs.pkgExes;
 
-import java.io.PrintStream;
+
 
 public class Exe001 {
 
@@ -13,20 +13,6 @@ public class Exe001 {
 	}
 
 	private void go() {
-
-		try {
-			System.out.println("aki...1...");
-			try(PrintStream out2 = System.out){
-				System.out.println("aki...2...");
-				out2.println("aki...3...");
-				System.out.println(System.out.checkError());
-			}
-			System.out.println("aki...4...");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(System.out.checkError());
-
 	}
 
 
