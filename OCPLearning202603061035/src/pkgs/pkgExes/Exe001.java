bc6c65b87c1d0b34def0a1e9c27531d21e961f44
Administrador <Administrador@MMW10LAB.mshome.net>
2026-03-06 10:34:52 -0400

    remembering LocalDateTime

---
+++ OCPLearning202603061035/src/pkgs/pkgExes/Exe001.java
@@ -0,0 +1,21 @@
+package pkgs.pkgExes;
+
+import java.time.LocalDateTime;
+
+public class Exe001 {
+
+
+
+	public static void main(String[] args) {
+
+		new Exe001().go();
+
+	}
+
+	private void go() {
+		System.out.println(LocalDateTime.now());
+	}
+
+
+
+}
