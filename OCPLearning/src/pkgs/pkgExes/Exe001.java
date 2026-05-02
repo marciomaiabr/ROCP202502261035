cfdafc65f13217768fbdeaf2c1458f45bc515158
Marcio Maia <marcio.luis.maia@gmail.com>
2026-05-02 12:42:45 -0400

    remembering Path resolve

--- OCPLearning/src/pkgs/pkgExes/Exe001.java
+++ OCPLearning/src/pkgs/pkgExes/Exe001.java
@@ -1,6 +1,7 @@
 package pkgs.pkgExes;
 
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Exe001 {
 
@@ -13,6 +14,81 @@ public class Exe001 {
 	}
 
 	private void go() {
+		Path pthis = null;
+		Path pparam = null;
+
+		//resolve=concatena a PathThis com a PathParam retornando PathThis\PathParam. SE PathParam is AbsolutePath então retorna PathParam INTACTA.
+
+		pthis = Paths.get("/d1/d12");
+		pparam = Paths.get("/d1/d12/d123/d1234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("/d1/d12/d123/d1234");
+		pparam = Paths.get("/d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("/d1/d12");
+		pparam = Paths.get("/d2/d22/d223/d2234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("/d2/d22/d223/d2234");
+		pparam = Paths.get("/d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		System.out.println();
+
+		pthis = Paths.get("d1/d12");
+		pparam = Paths.get("/d1/d12/d123/d1234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("d1/d12/d123/d1234");
+		pparam = Paths.get("/d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("d1/d12");
+		pparam = Paths.get("/d2/d22/d223/d2234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("d2/d22/d223/d2234");
+		pparam = Paths.get("/d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		System.out.println();
+
+		pthis = Paths.get("/d1/d12");
+		pparam = Paths.get("d1/d12/d123/d1234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("/d1/d12/d123/d1234");
+		pparam = Paths.get("d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("/d1/d12");
+		pparam = Paths.get("d2/d22/d223/d2234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("/d2/d22/d223/d2234");
+		pparam = Paths.get("d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		System.out.println();
+
+		pthis = Paths.get("d1/d12");
+		pparam = Paths.get("d1/d12/d123/d1234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("d1/d12/d123/d1234");
+		pparam = Paths.get("d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("d1/d12");
+		pparam = Paths.get("d2/d22/d223/d2234");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
+		pthis = Paths.get("d2/d22/d223/d2234");
+		pparam = Paths.get("d1/d12");
+		System.out.println("["+pthis+".resolve("+pparam+")="+(pthis.resolve(pparam))+"]"+"");
+
 	}
 
 
